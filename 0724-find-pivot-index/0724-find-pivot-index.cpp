41359bf69682fc91c5bb70a3c8f1af9b767b06e9
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-31 23:19:17 +0530

    Time: 68 ms (19.26%), Space: 31.1 MB (86.71%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        // int n = nums.length;
+        // int[] leftSum = new  int[n];        
+        // int[] rightSum = new  int[n];
+        // leftSum[0] = nums[0];
+        // rightSum[n-1] = nums[n-1];
+        // for(int i = 1; i < n; i++){
+        //     leftSum[i] = leftSum[i-1] + nums[i];
+        //     rightSum[n-i-1] = rightSum[n-i] + nums[n-i-1];
+        // }
+        // for(int i = 0; i < n; i++){
+        //     if(leftSum[i] == rightSum[i]){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        int n = nums.size();
+        int leftSum = 0;
+        int totalSum = 0;
+        for(int it: nums){
+            totalSum += it;
+        }
+        for(int i = 0; i < n; i++){
+            totalSum -= nums[i];
+            if(leftSum == totalSum){
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+};
