276fb04b3ce02d8367ab78a83e0e7b0f0ba7c6c5
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-31 23:13:26 +0530

    Time: 28 ms (87.19%), Space: 19.3 MB (14.05%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        // int i = 0;
+        // for(int it: nums){
+        //     if(it != 0){
+        //         nums[i++] = it;
+        //     }
+        // }
+        // while(i < nums.size()){
+        //     nums[i++] = 0;
+        // }
+        int i = 0;
+        for(int j = 0; j < nums.size(); j++){
+            if(nums[j] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+    }
+};
