1d49340c3293a31fd95a5aa9d0bf9f5d5c3c3fec
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-31 23:20:53 +0530

    Time: 47 ms (37.15%), Space: 19.6 MB (14.90%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // int ME = nums[0];
+        // int meCnt = 1;
+        // for(int i = 1; i < nums.size(); i++){
+        //     if(nums[i] == ME){
+        //         meCnt++;
+        //     }
+        //     else{
+        //         meCnt--;
+        //         if(meCnt == 0){
+        //             ME = nums[i];
+        //             meCnt = 1;
+        //         }
+        //     }
+        // }
+        // return ME;
+        int majorityIndex = 0;
+        int count = 1;
+        for(int index = 1; index < nums.size(); index++){
+            count = nums[majorityIndex] == nums[index] ? ++count : --count;
+            if(count == 0){
+                majorityIndex = index;
+                count = 1;
+            }
+        }
+        return nums[majorityIndex];
+    }
+};
