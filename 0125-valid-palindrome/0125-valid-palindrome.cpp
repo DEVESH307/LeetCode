6e642eff32cd378627bbb04bd1b12e06b239b6ea
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-01-25 22:21:58 +0530

    Time: 10 ms (42.81%), Space: 7.3 MB (92.97%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string str;
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] >= 'a' && s[i] <= 'z' || s[i] >= '0' && s[i] <= '9'){
-                str.push_back(s[i]);
-            }
-            else if(s[i] >= 'A' && s[i] <= 'Z'){
-                str.push_back(s[i]+32);
-            }
-            else{
-                continue;
-            }
-        }
         int start = 0;
-        int end = str.size()-1;
-        while(start <= end){
-            if(str[start] != str[end]){
+        int end = s.size()-1;
+        while(start < end){
+            while(start < end && !isalnum(s[start])) start++;
+            while(start < end && !isalnum(s[end])) end--;
+            
+            if(tolower(s[start++]) != tolower(s[end--])){
                 return false;
-            }
-            else{
-                start++;
-                end--;
             }
         }
         return true;
