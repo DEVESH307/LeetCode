bc413b54268c12589eb751085c8cb926d4d9cb3a
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-01-25 20:09:51 +0530

    Time: 23 ms (42.01%), Space: 6 MB (56.92%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -8,8 +8,6 @@ public:
             reverse = reverse*10+temp%10;
             temp /= 10;
         }
-        // if(n < 0) reverse *= -1;
-        // cout<<reverse<<endl;
         if(reverse == n) return true;
         else return false;
     }
