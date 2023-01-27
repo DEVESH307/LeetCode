874c3746a8566c7ef326c1f4aca0bdf873be8c28
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-01-27 22:23:01 +0530

    Time: 3 ms (62.13%), Space: 8.5 MB (6.96%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
+    // bool check(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<int> temp;
+    //     int i;
+    //     for(i = 1; i < n; i++){
+    //         if(nums[i] < nums[i-1]){
+    //             break;
+    //         }
+    //     }
+    //     int breakPoint = i;
+    //     while(i < n) temp.push_back(nums[i++]);
+    //     for(int i = 0; i < breakPoint; i++){
+    //         temp.push_back(nums[i]);
+    //     }
+    //     bool ans = true;
+    //     for(int i = 1; i < n; i++){
+    //         if(temp[i-1] > temp[i]){
+    //             ans = false;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
     bool check(vector<int>& nums) {
         int n = nums.size();
-        vector<int> temp;
-        int i;
-        for(i = 1; i < n; i++){
-            if(nums[i] < nums[i-1]){
-                break;
+        int break_point_count = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] > nums[(i+1)%n]){
+                break_point_count++;
             }
         }
-        int breakPoint = i;
-        while(i < n) temp.push_back(nums[i++]);
-        for(int i = 0; i < breakPoint; i++){
-            temp.push_back(nums[i]);
-        }
-        bool ans = true;
-        for(int i = 1; i < n; i++){
-            if(temp[i-1] > temp[i]){
-                ans = false;
-            }
-        }
-        return ans;
+        return break_point_count <= 1;
     }
 };
