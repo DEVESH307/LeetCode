32c1fa906f1cfea069b04f15ab4e7d96dcb50e0c
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-01-29 01:29:41 +0530

    Time: 241 ms (68.59%), Space: 134.6 MB (41.18%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
-    // vector<int> rearrangeArray(vector<int>& nums) {
-    //     // code here
-    //     int n = nums.size();
-    // vector<int> pos;
-    // vector<int> neg;
-    // for(int i = 0; i < n; i++){
-    // if(nums [i] >= 0){
-    // pos.push_back(nums [i]);
-    // }
-    // else{
-    // neg.push_back(nums [i]);
-    // }
-    // }
-    // int i, index;
-    // index = 0;
-    // for(i = 0; i < min(pos.size(), neg.size()); i++){
-    // nums [index++] = pos[i];
-    // nums [index++] = neg[i];
-    // }
-    // while(i < pos.size()){
-    // nums [index++] = pos[i++];
-    // }
-    // while(i < neg.size()){
-    // nums [index++] = neg[i++];
-    // }
-    //     return nums;
-    // }
-    
     vector<int> rearrangeArray(vector<int>& nums) {
+        // code here
         int n = nums.size();
-        vector<int> ans(n,0);
-        int indexpos = 0, indexneg = 1;
-        for(auto num: nums){
-            if(num > 0){
-                ans[indexpos] = num;
-                indexpos += 2;
+        vector<int> pos;
+        vector<int> neg;
+        for(int i = 0; i < n; i++){
+            if(nums[i] >= 0){
+                pos.push_back(nums[i]);
             }
-            if(num<0){
-                ans[indexneg] = num;
-                indexneg += 2;
+            else{
+                neg.push_back(nums[i]);
             }
         }
-        return ans;
+        int pos_idx = 0;
+        int neg_idx = 0;
+        int index = 0;
+        while(index < n){
+           if(pos_idx != pos.size()){
+               nums[index++] = pos[pos_idx++];
+           }
+           if(neg_idx != neg.size()){
+               nums[index++] = neg[neg_idx++];
+           }
+        }
+        return nums;
     }
+    
+    // vector<int> rearrangeArray(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<int> ans(n,0);
+    //     int indexpos = 0, indexneg = 1;
+    //     for(auto num: nums){
+    //         if(num > 0){
+    //             ans[indexpos] = num;
+    //             indexpos += 2;
+    //         }
+    //         if(num<0){
+    //             ans[indexneg] = num;
+    //             indexneg += 2;
+    //         }
+    //     }
+    //     return ans;
+    // }
 };
