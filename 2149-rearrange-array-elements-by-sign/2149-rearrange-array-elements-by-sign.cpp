f8afe08c71ed32f33bd2fee3c62f252a14e4abb5
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-01-29 01:27:31 +0530

    Time: 227 ms (87.42%), Space: 123.8 MB (95.54%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,30 +1,47 @@
 class Solution {
 public:
+    // vector<int> rearrangeArray(vector<int>& nums) {
+    //     // code here
+    //     int n = nums.size();
+    // vector<int> pos;
+    // vector<int> neg;
+    // for(int i = 0; i < n; i++){
+    // if(nums [i] >= 0){
+    // pos.push_back(nums [i]);
+    // }
+    // else{
+    // neg.push_back(nums [i]);
+    // }
+    // }
+    // int i, index;
+    // index = 0;
+    // for(i = 0; i < min(pos.size(), neg.size()); i++){
+    // nums [index++] = pos[i];
+    // nums [index++] = neg[i];
+    // }
+    // while(i < pos.size()){
+    // nums [index++] = pos[i++];
+    // }
+    // while(i < neg.size()){
+    // nums [index++] = neg[i++];
+    // }
+    //     return nums;
+    // }
+    
     vector<int> rearrangeArray(vector<int>& nums) {
-        // code here
         int n = nums.size();
-	    vector<int> pos;
-	    vector<int> neg;
-	    for(int i = 0; i < n; i++){
-	        if(nums [i] >= 0){
-	            pos.push_back(nums [i]);
-	        }
-	        else{
-	            neg.push_back(nums [i]);
-	        }
-	    }
-	    int i, index;
-	    index = 0;
-	    for(i = 0; i < min(pos.size(), neg.size()); i++){
-	        nums [index++] = pos[i];
-	        nums [index++] = neg[i];
-	    }
-	    while(i < pos.size()){
-	        nums [index++] = pos[i++];
-	    }
-	    while(i < neg.size()){
-	        nums [index++] = neg[i++];
-	    }
-        return nums;
+        vector<int> ans(n,0);
+        int indexpos = 0, indexneg = 1;
+        for(auto num: nums){
+            if(num > 0){
+                ans[indexpos] = num;
+                indexpos += 2;
+            }
+            if(num<0){
+                ans[indexneg] = num;
+                indexneg += 2;
+            }
+        }
+        return ans;
     }
 };
