94b7900fd2c4e1b78690b5028af35a64379187d5
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-01-30 10:08:29 +0530

    Time: 121 ms (96.67%), Space: 93.3 MB (91.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,9 +3,11 @@ public:
     int maxProfit(vector<int>& prices) {
         int minSoFar = prices[0];
         int maxProfit = INT_MIN;
+        int profit;
         for(auto price: prices){
             minSoFar = min(minSoFar, price);
-            maxProfit = max(maxProfit, price-minSoFar);
+            profit = price-minSoFar;
+            maxProfit = max(maxProfit, profit);
         }
         return maxProfit;
     }
