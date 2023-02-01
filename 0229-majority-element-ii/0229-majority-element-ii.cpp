5d1f46c5705d9d7839d25267bdeff75fa847fe28
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-02-01 15:44:13 +0530

    Time: 8 ms (94.69%), Space: 15.9 MB (83.99%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,18 +1,65 @@
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//         vector<int> res;
+//         int n = nums.size();
+//         int required_count = n/3;
+//         unordered_map<int, int> mp;
+//         for(auto num: nums){
+//             mp[num]++;
+//         }
+//         for(auto itr: mp){
+//             if(itr.second > required_count){
+//                 res.push_back(itr.first);
+//             }
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> res;
         int n = nums.size();
-        int required_count = n/3;
-        unordered_map<int, int> mp;
-        for(auto num: nums){
-            mp[num]++;
-        }
-        for(auto itr: mp){
-            if(itr.second > required_count){
-                res.push_back(itr.first);
+        int majorityElem1 = -1;
+        int majorityElem2 = -1;
+        int count1 = 0;
+        int count2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == majorityElem1){
+                count1++;
+            }
+            else if(nums[i] == majorityElem2){
+                count2++;
+            }
+            else if(count1 == 0){
+                count1++;
+                majorityElem1 = nums[i];
+            }
+            else if(count2 == 0){
+                count2++;
+                majorityElem2 = nums[i];
+            }
+            
+            else{
+                count1--;
+                count2--;
             }
         }
+        int countMajorityElem1 = 0;
+        int countMajorityElem2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == majorityElem1){
+                countMajorityElem1++;
+            }
+            else if(nums[i] == majorityElem2){
+                countMajorityElem2++;
+            }
+        }
+        if(countMajorityElem1 > n/3) res.push_back(majorityElem1);
+        if(countMajorityElem2 > n/3) res.push_back(majorityElem2);
+        // sort(res.begin(), res.end());
         return res;
     }
 };
