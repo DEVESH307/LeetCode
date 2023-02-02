70940bf23e142adec43bb320183b8751be111239
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-02-02 09:54:52 +0530

    Time: 32 ms (95.03%), Space: 27.5 MB (68.02%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,43 +1,44 @@
-// class Solution {
-//   public:
-//     int searchHelper(int nums[], int low, int high, int target) {
-//         // code here
-//         if(low > high) return -1;
-        
-//         int mid = (low+high)/2;
-//         if(nums[mid] == target) 
-//             return mid;
-//         else if(nums[mid] < target)
-//             return searchHelper(nums, mid+1, high, target);
-//         else
-//             return searchHelper(nums, low, mid-1, target);
-        
-//     }
-//     int search(int nums[], int n, int target) {
-//         // code here
-//         return searchHelper(nums, 0, n-1, target);
-//     }
-// };
-
-// Iteration Method
-
+// Using Recursion
 class Solution {
-    public:
+  public:
+    int searchHelper(vector<int>& nums, int low, int high, int target) {
+        // code here
+        if(low > high) return -1;
+        
+        int mid = (low+high)/2;
+        if(nums[mid] == target) 
+            return mid;
+        else if(nums[mid] < target)
+            return searchHelper(nums, mid+1, high, target);
+        else
+            return searchHelper(nums, low, mid-1, target);
+        
+    }
     int search(vector<int>& nums, int target) {
         // code here
         int n = nums.size();
-        int low = 0;
-        int high = n-1;
-        
-        while(low <= high){
-            int mid = (low+high)/2;
-            if(nums[mid] == target)
-                return mid;
-            else if(nums[mid] < target)
-                low = mid+1;
-            else
-                high = mid-1;
-        }
-        return -1;
+        return searchHelper(nums, 0, n-1, target);
     }
 };
+
+// Using Iteration
+// class Solution {
+//     public:
+//     int search(vector<int>& nums, int target) {
+//         // code here
+//         int n = nums.size();
+//         int low = 0;
+//         int high = n-1;
+        
+//         while(low <= high){
+//             int mid = (low+high)/2;
+//             if(nums[mid] == target)
+//                 return mid;
+//             else if(nums[mid] < target)
+//                 low = mid+1;
+//             else
+//                 high = mid-1;
+//         }
+//         return -1;
+//     }
+// };
