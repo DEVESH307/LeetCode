4c7e36b49c640fcb3115fd15444e390a01524134
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-02-18 01:50:51 +0530

    Time: 7 ms (37.1%), Space: 20.5 MB (55.39%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,15 +1,26 @@
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         xor_0_n = 0
+#         xor_nums = 0
+
+#         for i in range(n+1):
+#             xor_0_n ^= i
+
+#         for num in nums:
+#             xor_nums ^= num
+
+#         ans = xor_0_n ^ xor_nums
+
+#         return ans
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        xor_0_n = 0
-        xor_nums = 0
+        ans = n 
 
-        for i in range(n+1):
-            xor_0_n ^= i
-
-        for num in nums:
-            xor_nums ^= num
-
-        ans = xor_0_n ^ xor_nums
+        for i in range(n):
+            ans ^= i ^ nums[i]
 
         return ans
+
