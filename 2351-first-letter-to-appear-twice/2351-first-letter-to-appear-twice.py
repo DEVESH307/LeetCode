ee2b06c8e6d9dad3bfd92767bc8ba43e35267fa5
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-02-19 11:12:32 +0530

    Time: 0 ms (100%), Space: 19.1 MB (75.11%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
@@ -1,9 +1,19 @@
+# class Solution:
+#     def repeatedCharacter(self, s: str) -> str:
+#         st = set()
+
+#         for ch in s:
+#             if ch in st:
+#                 return ch
+#             else:
+#                 st.add(ch)
+
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
-        st = set()
+        freq = [0]*26
 
         for ch in s:
-            if ch in st:
+            if freq[ord(ch)-ord('a')] == 1:
                 return ch
             else:
-                st.add(ch)
+                freq[ord(ch)-ord('a')] += 1
