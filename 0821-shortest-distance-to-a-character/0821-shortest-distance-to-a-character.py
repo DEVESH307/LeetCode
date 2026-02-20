c45de593995fa9738203257a2097b0cbbe2d799e
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-02-20 10:07:27 +0530

    Time: 0 ms (100%), Space: 19.4 MB (55.1%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
@@ -1,26 +1,41 @@
+# class Solution:
+#     def shortestToChar(self, s: str, c: str) -> List[int]:
+#         n = len(s)
+#         res = [0]*n
+
+#         i = 0
+#         prev = float('-inf') # previous c index
+
+#         for j in range(n):
+#             if s[j] == c:
+#                 while i <= j:
+#                     res[i] = min(j - i, i - prev)
+#                     i += 1
+#                 prev = j
+        
+#         # remaining characters after last c
+#         while i < n:
+#             res[i] = i - prev
+#             i += 1
+
+#         return res
+        
+
 class Solution:
     def shortestToChar(self, s: str, c: str) -> List[int]:
         n = len(s)
-        res = [0]*n
+        res = [n] * n
 
-        i = 0
-        j = 0
-        prev = float('-inf') # previous c index
-
-        while j < n:
-            if s[j] != c:
-                j += 1
-            else:
-                while i <= j:
-                    res[i] = min(j - i, i - prev)
-                    i += 1
-                prev = j
-                j += 1
-        
-        # remaining characters after last c
-        while i < n:
+        prev = -n
+        for i in range(n):
+            if s[i] == c:
+                prev = i
             res[i] = i - prev
-            i += 1
+
+        prev = 2 * n
+        for i in range(n - 1, -1, -1):
+            if s[i] == c:
+                prev = i
+            res[i] = min(res[i], prev - i)
 
         return res
-        
