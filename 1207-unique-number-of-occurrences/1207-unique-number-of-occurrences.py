799a09bf5b6f96dcfbf8583bb118ad692763d4b2
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-02-20 12:28:54 +0530

    Time: 0 ms (100%), Space: 19.6 MB (7.75%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,3 +1,33 @@
+# class Solution:
+#     def uniqueOccurrences(self, arr: List[int]) -> bool:
+#         freq = {}
+
+#         for num in arr:
+#             freq[num] = freq.get(num, 0) + 1
+
+#         seen = set()
+
+#         for count in freq.values():
+#             if count in seen:
+#                 return False
+#             seen.add(count)
+
+#         return True
+
+
+# class Solution:
+#     def uniqueOccurrences(self, arr: List[int]) -> bool:
+#         freq = {}
+
+#         for num in arr:
+#             freq[num] = freq.get(num, 0) + 1
+
+#         occurrences = list(freq.values())
+#         unique_occurrences = set(occurrences)
+
+#         return len(occurrences) == len(unique_occurrences)
+
+
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         freq = {}
@@ -6,6 +36,4 @@ class Solution:
             freq[val] = freq.get(val, 0) + 1
 
         # for key, val in freq.items():
-        if len(freq.values()) != len(set(freq.values())):
-            return False
-        return True
+        return len(freq.values()) == len(set(freq.values()))
