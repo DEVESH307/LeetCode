19dfd54a59de4ed5535fd0afafcd99075958b889
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-02-20 10:56:34 +0530

    Time: 24 ms (94.03%), Space: 21.9 MB (47.32%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -16,6 +16,25 @@
         
 #         return ans
 
+
+# class Solution:
+#     def subarraySum(self, nums: List[int], k: int) -> int:
+#         n = len(nums)
+#         ps_count = {0: 1}
+#         curr_sum = 0
+#         ans = 0
+
+#         for num in nums:
+#             curr_sum += num
+
+#             if curr_sum - k in ps_count:
+#                 ans += ps_count[curr_sum-k]
+
+#             ps_count[curr_sum] = ps_count.get(curr_sum, 0) + 1
+        
+#         return ans
+
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         n = len(nums)
@@ -25,10 +44,7 @@ class Solution:
 
         for num in nums:
             curr_sum += num
-
-            if curr_sum - k in ps_count:
-                ans += ps_count[curr_sum-k]
-
+            ans += ps_count.get(curr_sum-k, 0)
             ps_count[curr_sum] = ps_count.get(curr_sum, 0) + 1
         
         return ans
