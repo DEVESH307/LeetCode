4611490c35e78f3ef1e8d530de7c130b99f89b3b
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-02-20 09:32:14 +0530

    Time: 0 ms (100%), Space: 19.2 MB (73.67%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        xor = x^y
+        ans  = 0
+        
+        while(xor):
+            xor = (xor) & (xor-1)
+            ans += 1
+
+        return ans
+        
