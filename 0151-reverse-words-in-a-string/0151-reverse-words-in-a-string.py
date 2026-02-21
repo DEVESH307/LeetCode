2bda2ba5f6bdfa9d31453b3fe20d6777ac100637
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-02-22 00:51:38 +0530

    Time: 0 ms (100%), Space: 19.5 MB (9.6%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,23 +1,29 @@
+# class Solution:
+#     def reverse(self, arr, i, j):
+#         while i < j:
+#             arr[i], arr[j] = arr[j], arr[i]
+#             i += 1
+#             j -= 1
+
+#     def reverseWords(self, s: str) -> str:
+#         s = " ".join(s.split())
+#         n = len(s)
+#         arr = list(s)
+
+#         self.reverse(arr, 0, n-1)
+
+#         i = 0
+#         for j in range(n):
+#             if arr[j] == " ":
+#                 self.reverse(arr, i, j-1)
+#                 i = j+1
+
+#         self.reverse(arr, i, n-1)
+
+#         return "".join(arr)
+
+
 class Solution:
-    def reverse(self, arr, i, j):
-        while i < j:
-            arr[i], arr[j] = arr[j], arr[i]
-            i += 1
-            j -= 1
-
     def reverseWords(self, s: str) -> str:
-        s = " ".join(s.split())
-        n = len(s)
-        arr = list(s)
-
-        self.reverse(arr, 0, n-1)
-
-        i = 0
-        for j in range(n):
-            if arr[j] == " ":
-                self.reverse(arr, i, j-1)
-                i = j+1
-
-        self.reverse(arr, i, n-1)
-
-        return "".join(arr)
+        words = s.split()
+        return " ".join(words[::-1])
