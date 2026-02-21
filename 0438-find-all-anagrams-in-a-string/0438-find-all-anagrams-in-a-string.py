96621ae09c04a726fb4edb049f2b9b617b605dd9
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-02-22 03:41:51 +0530

    Time: 37 ms (67.37%), Space: 19.8 MB (55.04%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -12,34 +12,104 @@
 #         return res
             
 
+# class Solution:
+#     def findAnagrams(self, s: str, p: str) -> List[int]:
+#         n = len(s)
+#         m = len(p)
+#         if m > n: return []
+        
+#         res = []
+        
+#         p_count = [0]*26
+#         window = [0]*26
+
+#         # build freq of p
+#         for ch in p:
+#             p_count[ord(ch) - ord('a')] += 1
+
+#         # first window
+#         for i in range(m):
+#             window[ord(s[i]) - ord('a')] += 1
+
+#         if window == p_count:
+#             res.append(0)
+
+#         # slide window
+#         for i in range(1, n-m+1):
+#             window[ord(s[i-1]) - ord('a')] -= 1
+#             window[ord(s[i+m-1]) - ord('a')] += 1
+
+#             if window == p_count:
+#                 res.append(i)
+
+#         return res
+
+
+# class Solution:
+#     def findAnagrams(self, s: str, p: str):
+
+#         n, m = len(s), len(p)
+#         if m > n:
+#             return []
+
+#         result = []
+
+#         p_count = [0] * 26
+#         window = [0] * 26
+
+#         # frequency of pattern
+#         for ch in p:
+#             p_count[ord(ch) - ord('a')] += 1
+
+#         left = 0
+
+#         for right in range(n):
+#             # include current character
+#             window[ord(s[right]) - ord('a')] += 1
+
+#             # maintain window size = m
+#             if right - left + 1 > m:
+#                 window[ord(s[left]) - ord('a')] -= 1
+#                 left += 1
+
+#             # check anagram
+#             if window == p_count:
+#                 result.append(left)
+
+#         return result
+
+
 class Solution:
-    def findAnagrams(self, s: str, p: str) -> List[int]:
-        n = len(s)
-        m = len(p)
-        if m > n: return []
-        
+    def findAnagrams(self, s: str, p: str):
+        n, m = len(s), len(p)
+        if m > n:
+            return []
+
         res = []
-        
-        p_count = [0]*26
-        window = [0]*26
 
-        # build freq of p
+        p_freq = {}
         for ch in p:
-            p_count[ord(ch) - ord('a')] += 1
+            p_freq[ch] = p_freq.get(ch, 0) + 1
 
-        # first window
+        window = {}
+        # build first window
         for i in range(m):
-            window[ord(s[i]) - ord('a')] += 1
+            window[s[i]] = window.get(s[i], 0) + 1
 
-        if window == p_count:
+        if window == p_freq:
             res.append(0)
 
         # slide window
-        for i in range(1, n-m+1):
-            window[ord(s[i-1]) - ord('a')] -= 1
-            window[ord(s[i+m-1]) - ord('a')] += 1
+        for i in range(m, n):
+            window[s[i]] = window.get(s[i], 0) + 1
 
-            if window == p_count:
-                res.append(i)
+            left_char = s[i - m]
+            window[left_char] -= 1
+
+            if window[left_char] == 0:
+                del window[left_char]
+
+            if window == p_freq:
+                res.append(i - m + 1)
 
         return res
