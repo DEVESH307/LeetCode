e66d1e8de88ed5bcc5180d9b81506a33ae9bf88e
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-02-22 03:10:33 +0530

    Time: 0 ms (100%), Space: 19.4 MB (54.57%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -1,14 +1,24 @@
+# class Solution:
+#     def areOccurrencesEqual(self, s: str) -> bool:
+#         n = len(s)
+#         freq = {}
+
+#         for ch in s:
+#             freq[ch] = freq.get(ch, 0) + 1
+
+#         count = freq[s[0]]
+#         for key, value in freq.items():
+#             if value != count:
+#                 return False
+            
+#         return True
+
+
 class Solution:
     def areOccurrencesEqual(self, s: str) -> bool:
-        n = len(s)
         freq = {}
 
         for ch in s:
             freq[ch] = freq.get(ch, 0) + 1
 
-        count = freq[s[0]]
-        for key, value in freq.items():
-            if value != count:
-                return False
-            
-        return True
+        return len(set(freq.values())) == 1
