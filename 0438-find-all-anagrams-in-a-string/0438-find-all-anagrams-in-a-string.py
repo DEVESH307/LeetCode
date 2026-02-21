b299abde86d1ffd655b7307a624469efb587db73
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-02-22 03:35:43 +0530

    Time: 24 ms (93.35%), Space: 19.7 MB (75.11%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,13 +1,45 @@
+# class Solution:
+#     def findAnagrams(self, s: str, p: str) -> List[int]:
+#         n = len(s)
+#         m = len(p)
+#         res = []
+#         target = sorted(p)
+
+#         for i in range(n-m+1):
+#             if sorted(s[i:i+m]) == target:
+#                 res.append(i)
+
+#         return res
+            
+
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         n = len(s)
         m = len(p)
+        if m > n: return []
+        
         res = []
-        target = sorted(p)
+        
+        p_count = [0]*26
+        window = [0]*26
 
-        for i in range(n-m+1):
-            if sorted(s[i:i+m]) == target:
+        # build freq of p
+        for ch in p:
+            p_count[ord(ch) - ord('a')] += 1
+
+        # first window
+        for i in range(m):
+            window[ord(s[i]) - ord('a')] += 1
+
+        if window == p_count:
+            res.append(0)
+
+        # slide window
+        for i in range(1, n-m+1):
+            window[ord(s[i-1]) - ord('a')] -= 1
+            window[ord(s[i+m-1]) - ord('a')] += 1
+
+            if window == p_count:
                 res.append(i)
 
         return res
-            
