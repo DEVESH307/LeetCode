80263d3d2a48bb139d3c4879e208d38524180f62
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-02-24 12:09:38 +0530

    Time: 62 ms (5.04%), Space: 28.3 MB (7.16%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,4 +1,23 @@
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         clean = ''.join(c.lower() for c in s if c.isalnum())
+#         return clean[::] == clean[::-1]
+
+
 class Solution:
+    def checkPalindrome(self, arr, i, j):
+        if i >= j:
+            return True
+
+        if arr[i] != arr[j]:
+            return False
+        
+        return self.checkPalindrome(arr, i+1, j-1)
+
+
     def isPalindrome(self, s: str) -> bool:
         clean = ''.join(c.lower() for c in s if c.isalnum())
-        return clean[::] == clean[::-1]
+        i = 0
+        j = len(clean)-1
+        return self.checkPalindrome(clean, i, j)
+        
