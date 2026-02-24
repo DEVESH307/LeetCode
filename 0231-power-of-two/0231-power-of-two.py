8800ba90c8c1e5bf2572808a5696c57ad958a1ff
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-02-25 02:08:30 +0530

    Time: 0 ms (100%), Space: 19.4 MB (47.18%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,10 +1,27 @@
+# class Solution:
+#     def isPowerOfTwo(self, n: int) -> bool:
+#        if n <= 0:
+#            return False
+#         pow2 = 1
+#         while pow2 <= n:
+#             if pow2 == n:
+#                 return True 
+#             pow2 <<= 1
+
+#         return False
+        
+
+# class Solution:
+#     def isPowerOfTwo(self, n: int) -> bool:
+#         if n <= 0:
+#             return False
+
+#         while n % 2 == 0:
+#             n //= 2
+
+#         return n == 1
+
+
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        pow2 = 1
-        while pow2 <= n:
-            if pow2 == n:
-                return True 
-            pow2 <<= 1
-
-        return False
-        
+        return n > 0 and (n & (n - 1)) == 0
