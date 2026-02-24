f808dcc9de3226b4f615137f26896299527d51a0
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-02-25 01:05:21 +0530

    Time: 0 ms (100%), Space: 19.4 MB (47.54%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,12 +1,18 @@
+# class Solution:
+#     def addDigits(self, num: int) -> int:
+#         if num < 10:
+#             return num
+
+#         digit_sum = 0
+#         while num:
+#             digit_sum += num % 10
+#             num //= 10
+
+#         return self.addDigits(digit_sum)
+        
+
 class Solution:
     def addDigits(self, num: int) -> int:
-        if num < 10:
-            return num
-
-        digit_sum = 0
-        while num:
-            digit_sum += num % 10
-            num //= 10
-
-        return self.addDigits(digit_sum)
-        
+        if num == 0: 
+            return 0
+        return 1 + (num-1)%9
