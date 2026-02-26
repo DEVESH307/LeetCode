8ba12663b09dc6f2badbb98fb74ee04293c4cb33
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-02-27 02:21:45 +0530

    Time: 0 ms (100%), Space: 23.9 MB (39.48%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -4,15 +4,43 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def countNodes(self, root: Optional[TreeNode]) -> int:
+#         def dfs(root):
+#             if not root:
+#                 return 0
+
+#             if not root.left and not root.right:
+#                 return 1
+
+#             return 1 + dfs(root.left) + dfs(root.right)
+
+#         return dfs(root)
+
+
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        def dfs(root):
-            if not root:
-                return 0
+        def left_height(node):
+            ht = 0
+            while node:
+                node = node.left
+                ht += 1
+            return ht
 
-            if not root.left and not root.right:
-                return 1
+        def right_height(node):
+            ht = 0
+            while node:
+                node = node.right
+                ht += 1
+            return ht
 
-            return 1 + dfs(root.left) + dfs(root.right)
+        if not root:
+            return 0
 
-        return dfs(root)
+        lh = left_height(root)
+        rh = right_height(root)
+
+        if lh == rh:
+            return (1 << lh) - 1
+
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
