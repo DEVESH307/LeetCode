6859f710295b749149f494e16713129dcea6cfcd
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-02-27 03:01:15 +0530

    Time: 8 ms (38.02%), Space: 26.2 MB (59.76%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -4,14 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+#         if not root:
+#             return 0
+
+#         total = 0
+#         if low <= root.val <= high:
+#             total = root.val
+        
+#         return total + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
+        
+
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         if not root:
             return 0
 
-        total = 0
-        if low <= root.val <= high:
-            total = root.val
+        if root.val < low:
+            return self.rangeSumBST(root.right, low, high)
         
-        return total + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
+        if root.val > high:
+            return self.rangeSumBST(root.left, low, high)
         
+        return root.val + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
