011d96da746497d2cf940a55b4e799453e807ef8
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-03-01 04:03:06 +0530

    Time: 449 ms (71.72%), Space: 47.5 MB (78.39%) - LeetHub

--- 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.py
+++ 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.py
@@ -1,18 +1,34 @@
+# class Solution:
+#     def subarrayBitwiseORs(self, arr: List[int]) -> int:
+#         prev = set()
+#         curr = set()
+#         res = set()
+
+#         for i, num in enumerate(arr):
+#             for x in prev:
+#                 curr.add(x | num)
+#                 res.add(x | num)
+
+#             curr.add(num)
+#             res.add(num)
+
+#             prev = curr.copy()
+#             curr.clear()
+
+#         return len(res)
+
+
 class Solution:
     def subarrayBitwiseORs(self, arr: List[int]) -> int:
         prev = set()
-        curr = set()
         res = set()
 
-        for i, num in enumerate(arr):
-            for x in prev:
-                curr.add(x | num)
-                res.add(x | num)
+        for num in arr:
+            curr = {num}
+            for val in prev:
+                curr.add(val | num)
 
-            curr.add(num)
-            res.add(num)
-
-            prev = curr.copy()
-            curr.clear()
+            res.update(curr)
+            prev = curr
 
         return len(res)
