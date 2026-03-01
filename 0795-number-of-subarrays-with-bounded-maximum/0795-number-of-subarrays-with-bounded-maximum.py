e0bb3701b5fa6f8390b28e572c93fdd47de2b7bf
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-03-01 12:50:41 +0530

    Time: 31 ms (70.98%), Space: 25.9 MB (25.65%) - LeetHub

---
+++ 0795-number-of-subarrays-with-bounded-maximum/0795-number-of-subarrays-with-bounded-maximum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
+        def count(bound):
+            ans = 0
+            length = 0
+
+            for num in nums:
+                if num <= bound:
+                    length += 1
+                else:
+                    length = 0
+
+                ans += length
+
+            return ans
+
+        return count(right) - count(left-1)
