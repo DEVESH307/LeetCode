b892a34122fea67b10ea163781550729eba89ae0
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-03-01 13:00:58 +0530

    Time: 27 ms (85.23%), Space: 25.4 MB (89.12%) - LeetHub

--- 0795-number-of-subarrays-with-bounded-maximum/0795-number-of-subarrays-with-bounded-maximum.py
+++ 0795-number-of-subarrays-with-bounded-maximum/0795-number-of-subarrays-with-bounded-maximum.py
@@ -1,17 +1,54 @@
+# class Solution:
+#     def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
+#         def count(bound):
+#             ans = 0
+#             length = 0
+
+#             for num in nums:
+#                 if num <= bound:
+#                     length += 1
+#                 else:
+#                     length = 0
+
+#                 ans += length
+
+#             return ans
+
+#         return count(right) - count(left-1)
+
+
+# class Solution:
+#     def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
+#         ans = 0
+#         last_valid = -1
+#         last_invalid = -1
+
+#         for i, num in enumerate(nums):
+#             if num > right:
+#                 last_invalid = i
+
+#             if left <= num <= right:
+#                 last_valid = i
+
+#             ans += max(0, last_valid-last_invalid)
+
+#         return ans
+
+
 class Solution:
     def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
-        def count(bound):
-            ans = 0
-            length = 0
+        ans = 0
+        count = 0
+        last_invalid = -1
 
-            for num in nums:
-                if num <= bound:
-                    length += 1
-                else:
-                    length = 0
+        for i, num in enumerate(nums):
+            if num > right:
+                last_invalid = i
+                count = 0
 
-                ans += length
+            if left <= num <= right:
+                count = i - last_invalid
 
-            return ans
+            ans += count            
 
-        return count(right) - count(left-1)
+        return ans
