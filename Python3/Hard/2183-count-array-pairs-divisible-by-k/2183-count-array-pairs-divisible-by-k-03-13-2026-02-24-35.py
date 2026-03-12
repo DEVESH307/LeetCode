33982c5830e1fadd4e6693eae879fbceae135b27
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-03-13 02:24:36 +0530

    N/A (0%) N/A (0%) 03-13-2026 2183-count-array-pairs-divisible-by-k Hard .py Array

---
+++ Python3/Hard/2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k-03-13-2026-02-24-35.py
@@ -0,0 +1,21 @@
+from math import gcd
+
+class Solution:
+    def countPairs(self, nums, k):
+        n = len(nums)
+        if k == 1:
+            return n * (n - 1) // 2
+            
+        count_gcd = {}
+        ans = 0
+
+        for num in nums:
+            curr_gcd = gcd(num, k)
+
+            for prev_gcd in count_gcd:
+                if (curr_gcd * prev_gcd) % k == 0:
+                    ans += count_gcd[prev_gcd]
+
+            count_gcd[curr_gcd] = count_gcd.get(curr_gcd, 0) + 1
+
+        return ans
