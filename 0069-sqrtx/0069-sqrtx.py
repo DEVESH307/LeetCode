9ad281fe901c060903b7090f022dc066b9d1481a
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-03-20 04:21:00 +0530

    Time: 1 ms (80.31%), Space: 19.2 MB (95.32%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        N = 100000
+        left, right = 0, min(x, N)
+        ans = 0
+
+        while left <= right:
+            mid = (left+right)//2
+
+            if mid*mid <= x:
+                ans = mid
+                left = mid+1
+            else:
+                right = mid-1
+
+        return ans
+        
