08349147952ce3791892f771d1c16a1f3104eed6
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-03-20 15:24:24 +0530

    Time: 0 ms (100%), Space: 19.6 MB (54.79%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,3 +1,33 @@
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> bool:
+#         left, right = 0, len(nums) - 1
+
+#         while left <= right:
+#             mid = (left + right) // 2
+
+#             if nums[mid] == target:
+#                 return True
+
+#             # handle duplicates (critical)
+#             if nums[left] == nums[mid] == nums[right]:
+#                 left += 1
+#                 right -= 1
+
+#             elif nums[left] <= nums[mid]:  # left half sorted
+#                 if nums[left] <= target < nums[mid]:
+#                     right = mid - 1
+#                 else:
+#                     left = mid + 1
+
+#             else:  # right half sorted
+#                 if nums[mid] < target <= nums[right]:
+#                     left = mid + 1
+#                 else:
+#                     right = mid - 1
+
+#         return False
+
+
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
         left, right = 0, len(nums) - 1
@@ -8,21 +38,18 @@ class Solution:
             if nums[mid] == target:
                 return True
 
-            # handle duplicates (critical)
-            if nums[left] == nums[mid] == nums[right]:
-                left += 1
-                right -= 1
-
-            elif nums[left] <= nums[mid]:  # left half sorted
+            # left portion
+            if nums[left] < nums[mid]:
                 if nums[left] <= target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
-
-            else:  # right half sorted
+            elif nums[left] > nums[mid]:
                 if nums[mid] < target <= nums[right]:
                     left = mid + 1
                 else:
                     right = mid - 1
+            else:
+                left += 1
 
         return False
