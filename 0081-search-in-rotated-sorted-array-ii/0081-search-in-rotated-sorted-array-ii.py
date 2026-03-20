7f30f288409d7918dc29b6f42363a291eec4826e
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-03-20 05:37:51 +0530

    Time: 0 ms (100%), Space: 19.7 MB (54.79%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,35 +1,14 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        n = len(nums)
-        ans = False
+        left, right = 0, len(nums) - 1
 
-        # -------- 1. Find pivot --------
-        left, right = 0, n - 1
-        while left < right:
-            mid = (left + right) // 2
-
-            if nums[mid] >= nums[right]:
-                left = mid+1
-            else:
-                right = mid
-
-        pivot = left
-        
-        # -------- 2. Choose search space --------
-        if target >= nums[pivot] and target <= nums[n-1]:
-            left, right = pivot, n-1
-        else:
-            left, right = 0, pivot-1
-
-         # -------- 3. Standard binary search --------
         while left <= right:
             mid = (left + right) // 2
 
             if nums[mid] == target:
-                ans = True
-                break
+                return True
 
-            # handle duplicates
+            # handle duplicates (critical)
             if nums[left] == nums[mid] == nums[right]:
                 left += 1
                 right -= 1
@@ -45,5 +24,5 @@ class Solution:
                     left = mid + 1
                 else:
                     right = mid - 1
-        
-        return ans
+
+        return False
