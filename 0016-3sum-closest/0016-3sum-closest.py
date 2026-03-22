27d5e5722c45d8ce60cc9f5f074f27d93e7580e2
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-03-23 01:57:46 +0530

    Time: 5 ms (98.32%), Space: 19.5 MB (13.14%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -7,6 +7,20 @@ class Solution:
         for i in range(n - 2):
             left, right = i + 1, n - 1
 
+            # pruning (minimum possible sum for this i)
+            min_sum = nums[i] + nums[i + 1] + nums[i + 2]
+            if min_sum >= target:
+                if abs(min_sum - target) < abs(closest_ans - target):
+                    closest_ans = min_sum
+                break
+
+            # pruning (maximum possible sum for this i)
+            max_sum = nums[i] + nums[n - 1] + nums[n - 2]
+            if max_sum <= target:
+                if abs(max_sum - target) < abs(closest_ans - target):
+                    closest_ans = max_sum
+                continue
+
             while left < right:
                 total = nums[i] + nums[left] + nums[right]
                 
