16a8b2b1d97b984719e6cbf51f30486952b7a4f1
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-03-22 20:24:00 +0530

    Time: 11 ms (29.06%), Space: 20.4 MB (93.06%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
@@ -36,6 +36,34 @@
 #         return len(pairs)
 
 
+# class Solution:
+#     def findPairs(self, nums: List[int], k: int) -> int:
+#         nums.sort()
+#         n = len(nums)
+#         left = 0
+#         right = 1
+#         ans = 0
+#         pair = set()
+
+#         while right < n :
+#             if left == right:
+#                 right += 1
+#                 continue
+
+#             diff = nums[right] - nums[left]
+
+#             if diff == k:
+#                 pair.add((nums[left], nums[right]))
+#                 left += 1
+#                 right += 1
+#             elif diff < k:
+#                 right += 1
+#             else:
+#                 left += 1
+
+#         return len(pair)
+
+
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
         nums.sort()
@@ -43,7 +71,6 @@ class Solution:
         left = 0
         right = 1
         ans = 0
-        pair = set()
 
         while right < n :
             if left == right:
@@ -53,13 +80,20 @@ class Solution:
             diff = nums[right] - nums[left]
 
             if diff == k:
-                pair.add((nums[left], nums[right]))
-                left += 1
-                right += 1
+                ans += 1
+                left_val = nums[left]
+                right_val = nums[right]
+
+                # skip duplicates
+                while left < n and nums[left] == left_val:
+                    left += 1
+                while right < n and nums[right] == right_val:
+                    right += 1
+
             elif diff < k:
                 right += 1
             else:
                 left += 1
 
-        return len(pair)
+        return ans
 
