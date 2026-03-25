b512f9cf249fedec08d42fb55b503a124279232a
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-03-26 01:12:35 +0530

    Time: 0 ms (100%), Space: 19.4 MB (10.79%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.py
@@ -0,0 +1,32 @@
+class Solution:
+    def compute_lps(self, s):
+        n = len(s)
+        LPS = [0] * n
+
+        for i in range(1, n):
+            x = LPS[i-1]
+
+            while x > 0 and s[i] != s[x]:
+                x = LPS[x-1]
+
+            if s[i] == s[x]:
+                x += 1
+
+            LPS[i] = x 
+
+        return LPS
+
+
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+            
+        n = len(s)
+        ans = 0
+        string = goal + '$' + s + s
+        LPS = self.compute_lps(string)
+
+        for i in range(len(LPS)):
+            if LPS[i] == n:
+                return True
+        return False
