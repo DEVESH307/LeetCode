311abf853807900d338e8ad950296d09cdfc437c
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-03-26 15:50:00 +0530

    Time: 0 ms (100%), Space: 19.3 MB (55.04%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -9,8 +9,8 @@ class Solution:
             return head
 
         cur = head
-        while cur and cur.next:
-            if cur.val == cur.next.val:
+        while cur:
+            if cur.next and cur.val == cur.next.val:
                 cur.next = cur.next.next
             else:
                 cur = cur.next
