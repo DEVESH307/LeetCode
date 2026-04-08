681b67c30f0b37f89d739081372e341a788ef1e5
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-09 04:57:19 +0530

    Time: 126 ms (10.47%), Space: 23.8 MB (22.5%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
@@ -4,39 +4,69 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def merge(self, arr1, arr2):
-        i = j = 0
-        merged = []
 
-        while i < len(arr1) and j < len(arr2):
-            if arr1[i] <= arr2[j]:
-                merged.append(arr1[i])
-                i += 1
-            else:
-                merged.append(arr2[j])
-                j += 1
+# class Solution:
+#     def merge(self, arr1, arr2):
+#         i = j = 0
+#         merged = []
 
-        # remaining
-        merged.extend(arr1[i:])
-        merged.extend(arr2[j:])
+#         while i < len(arr1) and j < len(arr2):
+#             if arr1[i] <= arr2[j]:
+#                 merged.append(arr1[i])
+#                 i += 1
+#             else:
+#                 merged.append(arr2[j])
+#                 j += 1
 
-        return merged
+#         # remaining
+#         merged.extend(arr1[i:])
+#         merged.extend(arr2[j:])
+
+#         return merged
 
         
+#     def getAllElements(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> List[int]:
+#         def inorder(node, sorted_arr):
+#             if not node:
+#                 return
+
+#             inorder(node.left, sorted_arr)
+#             sorted_arr.append(node.val)
+#             inorder(node.right, sorted_arr)
+
+#         arr1 = []
+#         arr2 = []
+
+#         inorder(root1, arr1)
+#         inorder(root2, arr2)
+
+#         return self.merge(arr1, arr2)
+
+
+class Solution:
     def getAllElements(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> List[int]:
-        def inorder(node, sorted_arr):
+
+        def inorder(node):
             if not node:
-                return
+                return []
+            return inorder(node.left) + [node.val] + inorder(node.right)
 
-            inorder(node.left, sorted_arr)
-            sorted_arr.append(node.val)
-            inorder(node.right, sorted_arr)
+        def merge(arr1, arr2):
+            i = j = 0
+            merged = []
 
-        arr1 = []
-        arr2 = []
+            while i < len(arr1) and j < len(arr2):
+                if arr1[i] <= arr2[j]:
+                    merged.append(arr1[i])
+                    i += 1
+                else:
+                    merged.append(arr2[j])
+                    j += 1
 
-        inorder(root1, arr1)
-        inorder(root2, arr2)
+            # remaining
+            merged.extend(arr1[i:])
+            merged.extend(arr2[j:])
 
-        return self.merge(arr1, arr2)
+            return merged
+
+        return merge(inorder(root1), inorder(root2))
