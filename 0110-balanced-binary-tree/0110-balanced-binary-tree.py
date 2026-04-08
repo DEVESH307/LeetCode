12a8b82bced37db1e43fd860983aa94d520c8407
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-09 02:25:14 +0530

    Time: 7 ms (16.68%), Space: 20.4 MB (86.12%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -4,26 +4,56 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def height(self, root):
-        if not root:
-            return 0
 
-        left_height = self.height(root.left)
-        right_height = self.height(root.right)
+# class Solution:
+#     def height(self, root):
+#         if not root:
+#             return 0
 
-        return 1 + max(left_height, right_height)
+#         left_height = self.height(root.left)
+#         right_height = self.height(root.right)
+
+#         return 1 + max(left_height, right_height)
 
         
+#     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+#         if not root:
+#             return True
+
+#         left_height = self.height(root.left)
+#         right_height = self.height(root.right)
+
+#         if abs(left_height - right_height) > 1:
+#             return False
+
+#         return self.isBalanced(root.left) and self.isBalanced(root.right)
+        
+
+# Binary tree node info
+class NodeInfo:
+    def __init__(self, balanced, height):
+        self.balanced = balanced
+        self.height = height 
+
+class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
+        def dfs(node):
+            if not node:
+                return NodeInfo(True, -1)
 
-        left_height = self.height(root.left)
-        right_height = self.height(root.right)
+            left = dfs(node.left)
+            right = dfs(node.right)
 
-        if abs(left_height - right_height) > 1:
-            return False
+            balanced = (
+                left.balanced and
+                right.balanced and
+                abs(left.height - right.height) <= 1
+            )
+            
+            height = max(left.height, right.height) + 1
 
-        return self.isBalanced(root.left) and self.isBalanced(root.right)
+            return NodeInfo(balanced, height)
+
+        return dfs(root).balanced
+
         
