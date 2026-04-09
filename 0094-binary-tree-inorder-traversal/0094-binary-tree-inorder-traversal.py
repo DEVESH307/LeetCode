6be44aeea91a2bc67e5866dfecce46ca5586ebd3
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-10 04:29:30 +0530

    Time: 0 ms (100%), Space: 19.2 MB (76.07%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -21,22 +21,47 @@
 #         return result
 
 
+# class Solution:
+#     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#         if not root:
+#             return []
+
+#         stack = []
+#         result = []
+#         node = root
+
+#         while node or stack:
+#             while node:
+#                 stack.append(node)
+#                 node = node.left
+
+#             node = stack.pop()
+#             result.append(node.val)
+#             node = node.right
+
+#         return result
+
+
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
-
-        stack = []
         result = []
-        node = root
+        curr = root
 
-        while node or stack:
-            while node:
-                stack.append(node)
-                node = node.left
+        while curr:
+            if not curr.left:
+                result.append(curr.val)
+                curr = curr.right
+            else:
+                pred = curr.left
+                while pred.right and pred.right != curr:
+                    pred = pred.right
 
-            node = stack.pop()
-            result.append(node.val)
-            node = node.right
+                if not pred.right:
+                    pred.right = curr
+                    curr = curr.left
+                else:
+                    pred.right = None
+                    result.append(curr.val)
+                    curr = curr.right
 
         return result
