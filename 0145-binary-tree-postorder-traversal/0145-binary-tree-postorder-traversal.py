e2ee1285b16342860ecbc8890ac4e1fe33171e24
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-10 04:21:44 +0530

    Time: 0 ms (100%), Space: 19.4 MB (43.19%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -4,17 +4,37 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#         result = []
+
+#         def dfs(node):
+#             if not node:
+#                 return
+
+#             dfs(node.left)
+#             dfs(node.right)
+#             result.append(node.val)
+
+#         dfs(root)
+#         return result
+
+
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        stack = [root]
         result = []
 
-        def dfs(node):
-            if not node:
-                return
-
-            dfs(node.left)
-            dfs(node.right)
+        while stack:
+            node = stack.pop()
             result.append(node.val)
 
-        dfs(root)
-        return result
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+
+        return result[::-1]
