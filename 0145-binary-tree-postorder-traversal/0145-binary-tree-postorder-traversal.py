0088027547fdf167d5c812ed034c9082faf36de9
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-10 04:35:14 +0530

    Time: 0 ms (100%), Space: 19.3 MB (43.19%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -20,21 +20,57 @@
 #         return result
 
 
+# class Solution:
+#     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#         if not root:
+#             return []
+
+#         stack = [root]
+#         result = []
+
+#         while stack:
+#             node = stack.pop()
+#             result.append(node.val)
+
+#             if node.left:
+#                 stack.append(node.left)
+#             if node.right:
+#                 stack.append(node.right)
+
+#         return result[::-1]
+
+
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
 
-        stack = [root]
+        def addPath(node):
+            start = node
+            path = []
+            while node:
+                path.append(node.val)
+                node = node.right
+            result.extend(path[::-1])
+
+        dummy = TreeNode(0)
+        dummy.left = root
+
         result = []
+        curr = dummy
 
-        while stack:
-            node = stack.pop()
-            result.append(node.val)
+        while curr:
+            if curr.left:
+                pred = curr.left
+                while pred.right and pred.right != curr:
+                    pred = pred.right
 
-            if node.left:
-                stack.append(node.left)
-            if node.right:
-                stack.append(node.right)
+                if not pred.right:
+                    pred.right = curr
+                    curr = curr.left
+                else:
+                    pred.right = None
+                    addPath(curr.left)
+                    curr = curr.right
+            else:
+                curr = curr.right
 
-        return result[::-1]
+        return result
