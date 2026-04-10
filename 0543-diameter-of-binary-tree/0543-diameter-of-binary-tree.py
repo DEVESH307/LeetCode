7e43d471ee42f8224c4de478248a5a0fa7c9f09e
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-11 03:31:02 +0530

    Time: 11 ms (7.32%), Space: 23.3 MB (6.45%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -4,25 +4,53 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def treeHeight(self, root):
-        if not root:
-            return -1
 
-        left_height = self.treeHeight(root.left)
-        right_height = self.treeHeight(root.right)
+# class Solution:
+#     def treeHeight(self, root):
+#         if not root:
+#             return -1
 
-        return 1 + max(left_height, right_height)
+#         left_height = self.treeHeight(root.left)
+#         right_height = self.treeHeight(root.right)
+
+#         return 1 + max(left_height, right_height)
 
 
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return -1
+#     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return -1
         
-        left_dia = self.diameterOfBinaryTree(root.left)
-        right_dia = self.diameterOfBinaryTree(root.right)
+#         left_dia = self.diameterOfBinaryTree(root.left)
+#         right_dia = self.diameterOfBinaryTree(root.right)
 
-        height_LST = self.treeHeight(root.left)
-        height_RST = self.treeHeight(root.right)
+#         height_LST = self.treeHeight(root.left)
+#         height_RST = self.treeHeight(root.right)
 
-        return max(left_dia, right_dia, height_LST + height_RST + 2)
+#         return max(left_dia, right_dia, height_LST + height_RST + 2)
+
+
+class NodeInfo:
+    def __init__(self, height, diameter):
+        self.height = height
+        self.diameter = diameter
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        return self.diameterOfBinaryTreeHelper(root).diameter
+
+    def diameterOfBinaryTreeHelper(self, root):
+        if not root:
+            return NodeInfo(-1, 0)
+
+        left = self.diameterOfBinaryTreeHelper(root.left)
+        right = self.diameterOfBinaryTreeHelper(root.right)
+
+        height = max(left.height, right.height) + 1
+        diameter = max(
+            left.diameter,
+            right.diameter,
+            left.height + right.height + 2
+        )
+
+        return NodeInfo(height, diameter)
