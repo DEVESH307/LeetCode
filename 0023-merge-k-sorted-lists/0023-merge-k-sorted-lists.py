71348de0d81f129eb36ed202ebd9a299e6e3e9f2
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-16 01:50:27 +0530

    Time: 8 ms (71.24%), Space: 23 MB (33.61%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,54 +3,57 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# # Appraoch 1 (O(n*k))
+# class Solution:
+#     def mergeTwoLists(self, head1, head2):
+#         dummy = ListNode(0)
+#         tail = dummy
+
+#         while head1 and head2:
+#             if head1.val < head2.val:
+#                 tail.next = head1
+#                 head1 = head1.next
+#             else:
+#                 tail.next = head2
+#                 head2 = head2.next
+#             tail = tail.next
+
+#         tail.next = head1 if head1 else head2
+
+#         return dummy.next  
+
+#     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+#         if not lists:
+#             return None
+#         if len(lists) == 1:
+#             return lists[0]
+
+#         return self.mergeTwoLists(lists[0], self.mergeKLists(lists[1:]))
+
+
+# # Appraoch 2: (O(nlogk))
+# class Solution:
+#     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+#         if not lists:
+#             return None
+
+#         while len(lists) > 1:
+#             merged = []
+
+#             for i in range(0, len(lists), 2):
+#                 l1 = lists[i]
+#                 l2 = lists[i + 1] if i + 1 < len(lists) else None
+#                 merged.append(self.mergeTwoLists(l1, l2))
+
+#             lists = merged
+
+#         return lists[0]
+
+
+# Appraoch 3: (O(nlogk))
+import heapq
 class Solution:
-    # def mergeTwoLists(self, head1, head2):
-    #     dummy = ListNode(0)
-    #     tail = dummy
-
-    #     while head1 and head2:
-    #         if head1.val < head2.val:
-    #             tail.next = head1
-    #             head1 = head1.next
-    #         else:
-    #             tail.next = head2
-    #             head2 = head2.next
-    #         tail = tail.next
-
-    #     tail.next = head1 if head1 else head2
-
-    #     return dummy.next  
-
-    # # Appraoch 1 (O(n*k))
-    # def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-    #     if not lists:
-    #         return None
-    #     if len(lists) == 1:
-    #         return lists[0]
-
-    #     return self.mergeTwoLists(lists[0], self.mergeKLists(lists[1:]))
-
-
-    # # Appraoch 2: (O(nlogk))
-    # def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-    #     if not lists:
-    #         return None
-
-    #     while len(lists) > 1:
-    #         merged = []
-
-    #         for i in range(0, len(lists), 2):
-    #             l1 = lists[i]
-    #             l2 = lists[i + 1] if i + 1 < len(lists) else None
-    #             merged.append(self.mergeTwoLists(l1, l2))
-
-    #         lists = merged
-
-    #     return lists[0]
-
-
-    # Appraoch 3: (O(nlogk))
-    import heapq
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         heap = []
 
