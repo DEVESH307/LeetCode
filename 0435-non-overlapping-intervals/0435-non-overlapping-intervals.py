73d84b1b46944ffa7ac95b1eb48fefcdc13a3a00
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-16 16:06:16 +0530

    Time: 56 ms (97.4%), Space: 49 MB (78.89%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,16 +1,31 @@
+# class Solution:
+#     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+#         intervals.sort(key=lambda x: x[0])
+#         prevEnd = intervals[0][1]
+#         ans = 0
+
+#         for start, end in intervals[1:]:
+#             if start >= prevEnd:
+#                 prevEnd = end
+#             else:
+#                 ans += 1
+#                 prevEnd = min(prevEnd, end)
+
+#         return ans
+         
+
+from typing import List
+
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key=lambda x: x[0])
-        prevEnd = intervals[0][1]
-        ans = 0
-
-        for start, end in intervals[1:]:
-            if start >= prevEnd:
-                prevEnd = end
-            else:
-                ans += 1
-                prevEnd = min(prevEnd, end)
-
-        return ans
-         
+        intervals.sort(key=lambda x: x[1])  # sort by end time
         
+        count = 0
+        last_end = float('-inf')
+        
+        for start, end in intervals:
+            if start >= last_end:
+                count += 1
+                last_end = end
+        
+        return len(intervals) - count
