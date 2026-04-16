d5b690da38777790422ce7ce82b9dd662fc8dbd7
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-17 01:26:46 +0530

    Time: 0 ms (100%), Space: 19.4 MB (72.99%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,16 +1,38 @@
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         # nums.sort()
+#         n = len(nums)
+#         result = []
+
+#         for i in range(1<<n):
+#             subset = []
+#             for j in range(n):
+#                 if i & (1 << j):
+#                     subset.append(nums[j])
+
+#             result.append(subset)
+
+#         # result.sort()
+#         return result
+
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         # nums.sort()
         n = len(nums)
         result = []
 
-        for i in range(1<<n):
-            subset = []
-            for j in range(n):
-                if i & (1 << j):
-                    subset.append(nums[j])
+        def dfs(curr_idx, curr_list):
+            if curr_idx == n:
+                result.append(curr_list[:])
+                return
 
-            result.append(subset)
+            curr_list.append(nums[curr_idx])
+            dfs(curr_idx + 1, curr_list)
+            curr_list.pop()
+            dfs(curr_idx + 1, curr_list)            
 
+        dfs(0, [])
         # result.sort()
         return result
+
