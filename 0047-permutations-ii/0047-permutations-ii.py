5953cda91fd10097e4346355abb73990a3918e7a
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-17 04:11:51 +0530

    Time: 2 ms (93.83%), Space: 19.8 MB (77.27%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,21 +1,51 @@
+# class Solution:
+#     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+#         n = len(nums)
+#         result = []
+
+#         def dfs(index):
+#             if index == n:
+#                 result.append(nums[:])
+#                 return
+#             used = set()
+#             for i in range(index, n):
+#                 if nums[i] in used:
+#                     continue
+#                 used.add(nums[i])
+
+#                 nums[index], nums[i] = nums[i], nums[index]
+#                 dfs(index + 1)
+#                 nums[i], nums[index] = nums[index], nums[i]
+
+#         dfs(0)
+#         return result                
+
+
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         n = len(nums)
+        used = [False] * n
         result = []
 
-        def dfs(index):
-            if index == n:
-                result.append(nums[:])
+        def dfs(path):
+            if len(path) == n:
+                result.append(path[:])
                 return
-            used = set()
-            for i in range(index, n):
-                if nums[i] in used:
+
+            for i in range(n):
+                if used[i]:
                     continue
-                used.add(nums[i])
 
-                nums[index], nums[i] = nums[i], nums[index]
-                dfs(index + 1)
-                nums[i], nums[index] = nums[index], nums[i]
+                # duplicate control
+                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                    continue
 
-        dfs(0)
-        return result                
+                used[i] = True
+                path.append(nums[i])
+                dfs(path)
+                path.pop()
+                used[i] = False
+
+        dfs([])
+        return result
