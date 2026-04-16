4f71490462df63f86742c1078dc42bf049344aef
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-17 03:06:25 +0530

    Time: 19 ms (52.33%), Space: 19.5 MB (49.73%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,3 +1,32 @@
+# class Solution:
+#     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+#         # candidates = sorted(set(candidates))
+#         candidates.sort()
+#         n = len(candidates)
+#         result = []
+
+#         def dfs(start, path, total):
+#             if total == target:
+#                 result.append(path[:])
+#                 return
+#             if total > target or start == n:
+#                 return
+
+#             path.append(candidates[start])
+#             dfs(start + 1, path, total + candidates[start])
+#             path.pop()
+
+#             # skip duplicates while excluding
+#             next_index = start + 1
+#             while next_index < n and candidates[next_index] == candidates[start]:
+#                 next_index += 1
+
+#             dfs(next_index, path, total)
+
+#         dfs(0, [], 0)
+#         return result
+
+
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         # candidates = sorted(set(candidates))
