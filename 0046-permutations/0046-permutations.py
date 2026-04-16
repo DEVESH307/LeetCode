8de17e16ab4e46de0d5c6cd3766572c9ea0b2586
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-17 04:10:56 +0530

    Time: 3 ms (31.63%), Space: 19.4 MB (70.51%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,17 +1,42 @@
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         n = len(nums)
+#         result = []
+
+#         def dfs(index):
+#             if index == n:
+#                 result.append(nums[:])
+#                 return
+
+#             for i in range(index, n):
+#                 nums[index], nums[i] = nums[i], nums[index]
+#                 dfs(index + 1)
+#                 nums[i], nums[index] = nums[index], nums[i]
+
+#         dfs(0)
+#         return result        
+
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         result = []
+        used = [False] * n
 
-        def dfs(index):
-            if index == n:
-                result.append(nums[:])
+        def dfs(path):
+            if len(path) == n:
+                result.append(path[:])
                 return
 
-            for i in range(index, n):
-                nums[index], nums[i] = nums[i], nums[index]
-                dfs(index + 1)
-                nums[i], nums[index] = nums[index], nums[i]
+            for i in range(n):
+                if used[i]:
+                    continue
 
-        dfs(0)
-        return result        
+                used[i] = True
+                path.append(nums[i])
+                dfs(path)
+                path.pop()
+                used[i] = False
+
+        dfs([])
+        return result
