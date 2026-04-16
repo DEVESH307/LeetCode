77e90ee3b0d3a7d858bf7ecc6cb2091496980112
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-17 01:46:02 +0530

    Time: 0 ms (100%), Space: 19.4 MB (72.99%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -37,29 +37,6 @@
 #         return result
 
 
-
-# class Solution:
-#     def subsets(self, nums: List[int]) -> List[List[int]]:
-#         # nums.sort()
-#         n = len(nums)
-#         result = []
-
-#         def dfs(index, path):
-#             if index == n:
-#                 result.append(path[:])
-#                 return
-
-#             path.append(nums[index])
-#             dfs(index + 1, path)
-#             path.pop()
-#             dfs(index + 1, path)            
-
-#         dfs(0, [])
-#         # result.sort()
-#         return result
-
-
-
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
