df1a1f9a6ece18f945867c69c1d8513c00351d30
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-19 02:13:39 +0530

    Time: 11 ms (46.77%), Space: 19.4 MB (59.06%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,83 @@
+# class Solution:
+#     def totalNQueens(self, n: int) -> int:
+#         cols = set() # columns
+#         diag1 = set() # (row - col)
+#         diag2 = set() # (row + col)
+#         count = 0
+
+#         def is_safe(row, col):
+#             if col in cols:
+#                 return False
+#             if (row - col) in diag1:
+#                 return False
+#             if (row + col) in diag2:
+#                 return False
+#             return True
+
+#         def dfs(row):
+#             nonlocal count
+
+#             if row == n:
+#                 count += 1
+#                 return
+
+#             for col in range(n):
+#                 if not is_safe(row, col):
+#                     continue
+
+#                 # place
+#                 cols.add(col)
+#                 diag1.add(row - col)
+#                 diag2.add(row + col)
+
+#                 dfs(row + 1)
+
+#                 # undo
+#                 cols.remove(col)
+#                 diag1.remove(row - col)
+#                 diag2.remove(row + col)
+
+#         dfs(0)
+#         return count
+
+
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        
+        cols = set()
+        diag1 = set()  # row - col
+        diag2 = set()  # row + col
+
+        def is_safe(row, col):
+            if col in cols:
+                return False
+            if (row - col) in diag1:
+                return False
+            if (row + col) in diag2:
+                return False
+            return True
+
+        def dfs(row):
+            if row == n:
+                return 1  # found valid arrangement
+
+            count = 0
+            for col in range(n):
+                if not is_safe(row, col):
+                    continue
+
+                # place
+                cols.add(col)
+                diag1.add(row - col)
+                diag2.add(row + col)
+
+                count += dfs(row + 1)
+
+                # undo
+                cols.remove(col)
+                diag1.remove(row - col)
+                diag2.remove(row + col)
+
+            return count
+
+        return dfs(0)
