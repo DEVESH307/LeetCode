bf7b43c06dfa0ac1892f0e0d3cb39705cda4f169
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-19 02:16:11 +0530

    Time: 11 ms (46.77%), Space: 19.5 MB (29.07%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,5 +1,51 @@
 # class Solution:
 #     def totalNQueens(self, n: int) -> int:
+#         result = []
+
+#         cols = set() # columns
+#         diag1 = set() # (row - col)
+#         diag2 = set() # (row + col)
+
+#         board = [['.']*n for _ in range(n)]
+
+#         def is_safe(row, col):
+#             if col in cols:
+#                 return False
+#             if (row - col) in diag1:
+#                 return False
+#             if (row + col) in diag2:
+#                 return False
+#             return True
+
+#         def dfs(row):
+#             if row == n:
+#                 result.append(["".join(r) for r in board])
+#                 return
+
+#             for col in range(n):
+#                 if not is_safe(row, col):
+#                     continue
+
+#                 # place
+#                 board[row][col] = 'Q'
+#                 cols.add(col)
+#                 diag1.add(row - col)
+#                 diag2.add(row + col)
+
+#                 dfs(row + 1)
+
+#                 # undo
+#                 board[row][col] = '.'
+#                 cols.remove(col)
+#                 diag1.remove(row - col)
+#                 diag2.remove(row + col)
+
+#         dfs(0)
+#         return len(result)
+
+
+# class Solution:
+#     def totalNQueens(self, n: int) -> int:
 #         cols = set() # columns
 #         diag1 = set() # (row - col)
 #         diag2 = set() # (row + col)
