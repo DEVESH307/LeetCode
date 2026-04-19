89371e6b92f9837161548d9e68525402abb06213
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-19 15:16:14 +0530

    Time: 35 ms (5.05%), Space: 19.4 MB (42.16%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,26 +1,54 @@
+# class Solution:
+#     def generateParenthesis(self, n: int) -> List[str]:
+#         # only add open paranthesis if open < n
+#         # only add a closing paranthesis if close < open
+#         # valid IFF open == closed == n
+
+#         stack = []
+#         res = []
+
+#         def backtrack(openN, closedN):
+#             if openN == closedN == n:
+#                 res.append("".join(stack))
+#                 return
+
+#             if openN < n:
+#                 stack.append("(")
+#                 backtrack(openN + 1, closedN)
+#                 stack.pop()
+
+#             if closedN < openN:
+#                 stack.append(")")
+#                 backtrack(openN, closedN + 1)
+#                 stack.pop()
+
+#         backtrack(0, 0)
+#         return res
+
+
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # only add open paranthesis if open < n
-        # only add a closing paranthesis if close < open
-        # valid IFF open == closed == n
-
-        stack = []
         res = []
 
-        def backtrack(openN, closedN):
-            if openN == closedN == n:
-                res.append("".join(stack))
+        def is_valid(s):
+            balance = 0
+            for ch in s:
+                if ch == '(':
+                    balance += 1
+                else:
+                    balance -= 1
+                if balance < 0:
+                    return False
+            return balance == 0
+
+        def dfs(s):
+            if len(s) == 2 * n:
+                if is_valid(s):
+                    res.append(s)
                 return
 
-            if openN < n:
-                stack.append("(")
-                backtrack(openN + 1, closedN)
-                stack.pop()
+            dfs(s + '(')
+            dfs(s + ')')
 
-            if closedN < openN:
-                stack.append(")")
-                backtrack(openN, closedN + 1)
-                stack.pop()
-
-        backtrack(0, 0)
+        dfs("")
         return res
