03dd4c24781a0fbf855f6597deac4fe5dfafd1ef
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-20 13:30:36 +0530

    Time: 457 ms (64%), Space: 33.2 MB (87.11%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.py
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.py
@@ -49,18 +49,39 @@
 #         return dfs(n)
 
 
+# class Solution:
+#     def climbStairs(self, n: int, costs: List[int]) -> int:
+#         costs = [0] + costs  # now costs[1..n]
+#         dp = [float('inf')] * (n + 1)
+#         dp[0] = 0
+
+#         for i in range(1, n+1):
+#             if i - 1 >=  0:
+#                 dp[i] = min(dp[i], dp[i-1] + costs[i] + 1)
+#             if i - 2 >=  0:
+#                 dp[i] = min(dp[i], dp[i-2] + costs[i] + 4)
+#             if i - 3 >=  0:
+#                 dp[i] = min(dp[i], dp[i-3] + costs[i] + 9)
+
+#         return dp[n]
+
+
 class Solution:
     def climbStairs(self, n: int, costs: List[int]) -> int:
         costs = [0] + costs  # now costs[1..n]
-        dp = [float('inf')] * (n + 1)
-        dp[0] = 0
+        dp0 = float('inf')   # dp[i-3] = dp[3]
+        dp1 = float('inf')   # dp[i-2] = dp[1]
+        dp2 = 0              # dp[i-1] = dp[0]
 
-        for i in range(n+1):
+        for i in range(1, n+1):
+            curr = float('inf')
             if i - 1 >=  0:
-                dp[i] = min(dp[i], dp[i-1] + costs[i] + 1)
+                curr = min(curr, dp2 + costs[i] + 1)
             if i - 2 >=  0:
-                dp[i] = min(dp[i], dp[i-2] + costs[i] + 4)
+                curr = min(curr, dp1 + costs[i] + 4)
             if i - 3 >=  0:
-                dp[i] = min(dp[i], dp[i-3] + costs[i] + 9)
+                curr = min(curr, dp0 + costs[i] + 9)
 
-        return dp[n]
+            dp0, dp1, dp2 = dp1, dp2, curr
+
+        return dp2
