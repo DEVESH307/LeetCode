56f222eebef43c55a3ec5709bd193d9be1f0b142
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-21 13:23:35 +0530

    Time: 0 ms (100%), Space: 19.3 MB (46.67%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -22,31 +22,71 @@
 #         return dfs(n)
 
 
+# class Solution:
+#     def numDecodings(self, s: str) -> int:
+#         n = len(s)
+#         dp = [-1] * (n + 1)
+        
+#         def dfs(i):
+#             # i = length of string considered
+#             if i == 0:
+#                 return 1
+
+#             if dp[i] != -1:
+#                 return dp[i]
+
+#             ways = 0
+
+#             # take one digit (only if not '0')
+#             if s[i - 1] != '0':
+#                 ways += dfs(i - 1)
+
+#             # take two digits (10–26)
+#             if i - 2 >= 0 and 10 <= int(s[i-2:i]) <= 26:
+#                 ways += dfs(i - 2)
+
+#             dp[i] = ways
+#             return dp[i]
+
+#         return dfs(n)
+
+
+# class Solution:
+#     def numDecodings(self, s: str) -> int:
+#         n = len(s)
+#         dp = [0] * (n + 1)
+#         dp[0] = 1 # empty string
+
+#         for i in range(1, n + 1):
+#             # take one digit (only if not '0')
+#             if s[i - 1] != '0':
+#                 dp[i] = dp[i] + dp[i - 1]
+
+#             # take two digits (10–26)
+#             if i - 2 >= 0 and 10 <= int(s[i-2:i]) <= 26:
+#                 dp[i] = dp[i] + dp[i - 2]
+
+#         return dp[n]
+
+
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
-        dp = [-1] * (n + 1)
-        
-        def dfs(i):
-            # i = length of string considered
-            if i == 0:
-                return 1
+        prev2 = 1 # dp[0] -> empty string
+        prev1 = 1 if s[0] != '0' else 0  # dp[1]
 
-            if dp[i] != -1:
-                return dp[i]
-
-            ways = 0
+        for i in range(2, n + 1):
+            curr = 0
 
             # take one digit (only if not '0')
             if s[i - 1] != '0':
-                ways += dfs(i - 1)
+                curr += prev1
 
             # take two digits (10–26)
-            if i - 2 >= 0 and 10 <= int(s[i-2:i]) <= 26:
-                ways += dfs(i - 2)
+            if 10 <= int(s[i-2:i]) <= 26:
+                curr += prev2
 
-            dp[i] = ways
-            return dp[i]
+            prev2 = prev1
+            prev1 = curr
 
-        return dfs(n)
-            
+        return prev1
