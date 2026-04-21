d93f8acf49105939fb2b3b96e0c2db2b95849ae6
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-22 03:01:06 +0530

    Time: 2497 ms (32.54%), Space: 22.9 MB (55.33%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -19,27 +19,46 @@
 #         return min(dfs(n - 1, j) for j in range(n))        
 
 
+# class Solution:
+#     def minFallingPathSum(self, grid: List[List[int]]) -> int:
+#         n = len(grid)
+#         dp = [[None] * n for _ in range(n)]
+
+#         def dfs(i, j):
+#             if j < 0 or j >= n:
+#                 return float('inf')
+
+#             if i == 0:
+#                 return grid[0][j]
+
+#             if dp[i][j] is not None:
+#                 return dp[i][j]
+
+#             res = float('inf')
+#             for k in range(n):
+#                 if k != j:
+#                     res = min(res, dfs(i - 1, k))
+
+#             dp[i][j] = grid[i][j] + res
+#             return dp[i][j]
+
+#         return min(dfs(n - 1, j) for j in range(n))
+
+
 class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        dp = [[None] * n for _ in range(n)]
+        dp = [[0] * n for _ in range(n)]
+        # base case
+        dp[0] = grid[0][:]
 
-        def dfs(i, j):
-            if j < 0 or j >= n:
-                return float('inf')
+        for i in range(1, n):
+            for j in range(n):
+                res = float('inf')
+                for k in range(n):
+                    if k != j:
+                        res = min(res, dp[i - 1][k])
 
-            if i == 0:
-                return grid[0][j]
+                dp[i][j] = grid[i][j] + res
 
-            if dp[i][j] is not None:
-                return dp[i][j]
-
-            res = float('inf')
-            for k in range(n):
-                if k != j:
-                    res = min(res, dfs(i - 1, k))
-
-            dp[i][j] = grid[i][j] + res
-            return dp[i][j]
-
-        return min(dfs(n - 1, j) for j in range(n))
+        return min(dp[n - 1])
