9c482b3321f3ac2e4accd220304a8235e5006571
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-22 02:54:37 +0530

    Time: 6702 ms (10.65%), Space: 30.9 MB (15.09%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -0,0 +1,45 @@
+# class Solution:
+#     def minFallingPathSum(self, grid: List[List[int]]) -> int:
+#         n = len(grid)
+
+#         def dfs(i, j):
+#             if j < 0 or j >= n:
+#                 return float('inf')
+
+#             if i == 0:
+#                 return grid[0][j]
+
+#             res = float('inf')
+#             for k in range(n):
+#                 if k != j:
+#                     res = min(res, dfs(i - 1, k))
+
+#             return grid[i][j] + res
+
+#         return min(dfs(n - 1, j) for j in range(n))        
+
+
+class Solution:
+    def minFallingPathSum(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        dp = [[None] * n for _ in range(n)]
+
+        def dfs(i, j):
+            if j < 0 or j >= n:
+                return float('inf')
+
+            if i == 0:
+                return grid[0][j]
+
+            if dp[i][j] is not None:
+                return dp[i][j]
+
+            res = float('inf')
+            for k in range(n):
+                if k != j:
+                    res = min(res, dfs(i - 1, k))
+
+            dp[i][j] = grid[i][j] + res
+            return dp[i][j]
+
+        return min(dfs(n - 1, j) for j in range(n))
