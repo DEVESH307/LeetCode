43366cc6a25b10649eb488b00246e3a6a3c1a15b
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-23 01:56:24 +0530

    Time: 39 ms (78.76%), Space: 19.3 MB (93.7%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -18,31 +18,88 @@
 #         return dfs(l1, l2)        
 
 
-class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        l1, l2 = len(word1), len(word2)
+# class Solution:
+#     def minDistance(self, word1: str, word2: str) -> int:
+#         l1, l2 = len(word1), len(word2)
         
-        dp = [[None] * (l2 + 1) for _ in range(l1 + 1)]
+#         dp = [[None] * (l2 + 1) for _ in range(l1 + 1)]
         
-        def dfs(i, j):
-            if i == 0:
-                return j
+#         def dfs(i, j):
+#             if i == 0:
+#                 return j
             
-            if j == 0:
-                return i
+#             if j == 0:
+#                 return i
             
-            if dp[i][j] is not None:
-                return dp[i][j]
+#             if dp[i][j] is not None:
+#                 return dp[i][j]
             
-            if word1[i-1] == word2[j-1]:
-                dp[i][j] = dfs(i-1, j-1)
-            else:
-                insert = dfs(i, j-1)
-                delete = dfs(i-1, j)
-                replace = dfs(i-1, j-1)
+#             if word1[i-1] == word2[j-1]:
+#                 dp[i][j] = dfs(i-1, j-1)
+#             else:
+#                 insert = dfs(i, j-1)
+#                 delete = dfs(i-1, j)
+#                 replace = dfs(i-1, j-1)
                 
-                dp[i][j] = 1 + min(insert, delete, replace)
+#                 dp[i][j] = 1 + min(insert, delete, replace)
             
-            return dp[i][j]
+#             return dp[i][j]
         
-        return dfs(l1, l2)
+#         return dfs(l1, l2)
+
+
+# class Solution:
+#     def minDistance(self, word1: str, word2: str) -> int:
+#         l1, l2 = len(word1), len(word2)
+        
+#         dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
+        
+#         # base cases
+#         for i in range(l1 + 1):
+#             dp[i][0] = i   # delete all chars
+        
+#         for j in range(l2 + 1):
+#             dp[0][j] = j   # insert all chars
+        
+#         # fill table
+#         for i in range(1, l1 + 1):
+#             for j in range(1, l2 + 1):
+#                 if word1[i-1] == word2[j-1]:
+#                     dp[i][j] = dp[i-1][j-1]
+#                 else:
+#                     insert = dp[i][j-1]
+#                     delete = dp[i-1][j]
+#                     replace = dp[i-1][j-1]
+                    
+#                     dp[i][j] = 1 + min(insert, delete, replace)
+        
+#         return dp[l1][l2]
+
+
+class Solution:
+    def minDistance(self, A, B):
+        # always use smaller string for columns (less space)
+        if len(A) < len(B):
+            A, B = B, A
+        
+        la, lb = len(A), len(B)
+        
+        prev = list(range(lb + 1))  # base row
+        
+        for i in range(1, la + 1):
+            curr = [0] * (lb + 1)
+            curr[0] = i
+            
+            for j in range(1, lb + 1):
+                if A[i-1] == B[j-1]:
+                    curr[j] = prev[j-1]
+                else:
+                    insert = curr[j-1]
+                    delete = prev[j]
+                    replace = prev[j-1]
+                    
+                    curr[j] = 1 + min(insert, delete, replace)
+            
+            prev = curr
+        
+        return prev[lb]
