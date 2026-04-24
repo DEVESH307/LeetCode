a2bf3d8ef717905f83ae3d10a66748e5b673b3be
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-25 02:28:09 +0530

    Time: 1142 ms (11.61%), Space: 34.3 MB (29.26%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -20,49 +20,23 @@
 #         return dfs(0, n)
 
 
-# class Solution:
-#     def minCost(self, n: int, cuts: List[int]) -> int:
-#         dp = {}
-
-#         def dfs(l, r):
-#             if r - l == 1:
-#                 return 0
-            
-#             if (l, r) in dp:
-#                 return dp[(l, r)]
-
-#             res = float('inf')
-#             for c in cuts:
-#                 if l < c < r:
-#                     res = min(res, (r - l) + dfs(l, c) + dfs(c, r))
-
-#             dp[(l, r)] = res = 0 if res == float('inf') else res
-#             return res
-
-#         return dfs(0, n)
-
-
 class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
-        cuts = [0] + sorted(cuts) + [n]
-        m = len(cuts)
-
-        dp = {}  # (l, r) -> min cost
+        dp = {}
 
         def dfs(l, r):
-            if r - l <= 1:
+            if r - l == 1:
                 return 0
-
+            
             if (l, r) in dp:
                 return dp[(l, r)]
 
             res = float('inf')
+            for c in cuts:
+                if l < c < r:
+                    res = min(res, (r - l) + dfs(l, c) + dfs(c, r))
 
-            for k in range(l + 1, r):
-                cost = (cuts[r] - cuts[l]) + dfs(l, k) + dfs(k, r)
-                res = min(res, cost)
-
-            dp[(l, r)] = res
+            dp[(l, r)] = res = 0 if res == float('inf') else res
             return res
 
-        return dfs(0, m - 1)
+        return dfs(0, n)
