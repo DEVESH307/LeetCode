e255a4e4c47302dfd7e235ba7369b77b5583ef65
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-25 23:49:00 +0530

    Time: 287 ms (97.82%), Space: 19.2 MB (95.69%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -0,0 +1,132 @@
+# class Solution:
+#     def minCut(self, s: str) -> int:
+#         n = len(s)
+
+#         def is_pal(l, r):
+#             while l < r:
+#                 if s[l] != s[r]:
+#                     return False
+#                 l += 1
+#                 r -= 1
+#             return True
+
+#         def dfs(i):
+#             if i == n:
+#                 return -1
+
+#             ans = float('inf')
+#             for j in range(i, n):
+#                 if is_pal(i, j):
+#                     ans = min(ans, 1 + dfs(j + 1))
+#             return ans
+
+#         return dfs(0)
+
+
+# class Solution:
+#     def minCut(self, s: str) -> int:
+#         n = len(s)
+#         dp = [-1] * n
+
+#         def is_pal(l, r):
+#             while l < r:
+#                 if s[l] != s[r]:
+#                     return False
+#                 l += 1
+#                 r -= 1
+#             return True
+
+#         def dfs(i):
+#             if i == n:
+#                 return -1
+
+#             if dp[i] != -1:
+#                 return dp[i]
+
+#             ans = float('inf')
+#             for j in range(i, n):
+#                 if is_pal(i, j):
+#                     ans = min(ans, 1 + dfs(j + 1))
+
+#             dp[i] = ans
+#             return ans
+
+#         return dfs(0)
+
+
+# class Solution:
+#     def minCut(self, s: str) -> int:
+#         n = len(s)
+
+#         def is_pal(l, r):
+#             while l < r:
+#                 if s[l] != s[r]:
+#                     return False
+#                 l += 1
+#                 r -= 1
+#             return True
+
+#         dp = [0] * (n + 1)
+#         dp[n] = -1
+
+#         for i in range(n - 1, -1, -1):
+#             ans = float('inf')
+#             for j in range(i, n):
+#                 if is_pal(i, j):
+#                     ans = min(ans, 1 + dp[j + 1])
+#             dp[i] = ans
+
+#         return dp[0]
+
+
+# class Solution:
+#     def minCut(self, s: str) -> int:
+#         n = len(s)
+
+#         # Precompute palindrome table
+#         pal = [[False] * n for _ in range(n)]
+#         for i in range(n - 1, -1, -1):
+#             for j in range(i, n):
+#                 if s[i] == s[j] and (j - i <= 2 or pal[i + 1][j - 1]):
+#                     pal[i][j] = True
+
+#         dp = [0] * (n + 1)
+#         dp[n] = -1
+
+#         for i in range(n - 1, -1, -1):
+#             ans = float('inf')
+#             for j in range(i, n):
+#                 if pal[i][j]:
+#                     ans = min(ans, 1 + dp[j + 1])
+#             dp[i] = ans
+
+#         return dp[0]
+
+
+class Solution:
+    def minCut(self, s: str) -> int:
+        n = len(s)
+        dp = [i for i in range(n)]  # worst case: i cuts
+
+        for center in range(n):
+            # odd length
+            l = r = center
+            while l >= 0 and r < n and s[l] == s[r]:
+                if l == 0:
+                    dp[r] = 0
+                else:
+                    dp[r] = min(dp[r], dp[l - 1] + 1)
+                l -= 1
+                r += 1
+
+            # even length
+            l, r = center, center + 1
+            while l >= 0 and r < n and s[l] == s[r]:
+                if l == 0:
+                    dp[r] = 0
+                else:
+                    dp[r] = min(dp[r], dp[l - 1] + 1)
+                l -= 1
+                r += 1
+
+        return dp[-1]
