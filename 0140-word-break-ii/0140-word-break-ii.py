7b73a991e74e863f69f38b5c82ca3cb0fc4d48fd
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-26 17:22:31 +0530

    Time: 0 ms (100%), Space: 19.3 MB (54.26%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        n = len(s)
+        word_set = set(wordDict)
+        result = []
+
+        def dfs(start, path):
+            # base case: reached end of string
+            if start == n:
+                result.append(" ".join(path))
+                return
+
+            # try every possible end index
+            for end in range(start + 1, n + 1):
+                word = s[start:end]
+                # if word in word_set and dfs(end):
+                #     dfs(end, path + [word])
+
+                if word in word_set:
+                    path.append(word)     # choose
+                    dfs(end, path)        # explore
+                    path.pop()            # undo (backtrack)
+
+        dfs(0, [])
+        return result
+                    
+        
