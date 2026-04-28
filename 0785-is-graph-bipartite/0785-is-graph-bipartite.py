55eaf78e3a44073d166350d3fe5a4e6c7b31ca0d
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-29 02:28:17 +0530

    Time: 0 ms (100%), Space: 20.6 MB (31.25%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,22 +1,48 @@
+# class Solution:
+#     def isBipartite(self, graph: List[List[int]]) -> bool:
+#         n = len(graph)
+#         color = [-1] * n  # -1 = unvisited
+#         for v in range(n):
+#             if color[v] != -1:
+#                 continue
+#             queue = deque([v])
+#             color[v] = 0
+
+#             while queue:
+#                 node = queue.popleft()
+
+#                 for nei in graph[node]:
+#                     if color[nei] == -1:
+#                         color[nei] = 1 - color[node]
+#                         queue.append(nei)
+#                     else:
+#                         if color[nei] == color[node]:
+#                             return False # conflict → not bipartite
+
+#         return True           
+
+
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         n = len(graph)
+
         color = [-1] * n  # -1 = unvisited
-        for v in range(n):
-            if color[v] != -1:
-                continue
-            queue = deque([v])
-            color[v] = 0
 
-            while queue:
-                node = queue.popleft()
+        def dfs(node, c):
+            color[node] = c
 
-                for nei in graph[node]:
-                    if color[nei] == -1:
-                        color[nei] = 1 - color[node]
-                        queue.append(nei)
-                    else:
-                        if color[nei] == color[node]:
-                            return False # conflict → not bipartite
+            for nei in graph[node]:
+                if color[nei] == -1:
+                    if not dfs(nei, 1 - c):
+                        return False
+                elif color[nei] == c:
+                    return False  # same color → odd cycle
 
-        return True           
+            return True
+
+        for i in range(n):
+            if color[i] == -1:
+                if not dfs(i, 0):
+                    return False  # odd cycle exists
+
+        return True  # no odd cycle → bipartite
