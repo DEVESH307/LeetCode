34315d8a9ecaeb554679537f3dcf12a88483bc83
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-29 02:25:55 +0530

    Time: 31 ms (69.73%), Space: 24.2 MB (32.92%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,27 +1,56 @@
+# class Solution:
+#     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+#         # build graph
+#         graph = defaultdict(list)
+#         for u, v in dislikes:
+#             graph[u].append(v)
+#             graph[v].append(u)
+
+#         color = [-1] * (n + 1)  # -1 = unvisited
+#         for v in range(1, n+1):
+#             if color[v] != -1:
+#                 continue
+#             queue = deque([v])
+#             color[v] = 1
+
+#             while queue:
+#                 node = queue.popleft()
+
+#                 for nei in graph[node]:
+#                     if color[nei] == -1:
+#                         color[nei] = 1 - color[node]
+#                         queue.append(nei)
+#                     else:
+#                         if color[nei] == color[node]:
+#                             return False # conflict → not bipartite
+
+#         return True        
+
+
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
-        # build graph
         graph = defaultdict(list)
         for u, v in dislikes:
             graph[u].append(v)
             graph[v].append(u)
 
         color = [-1] * (n + 1)  # -1 = unvisited
-        for v in range(1, n+1):
-            if color[v] != -1:
-                continue
-            queue = deque([v])
-            color[v] = 1
 
-            while queue:
-                node = queue.popleft()
+        def dfs(node, c):
+            color[node] = c
 
-                for nei in graph[node]:
-                    if color[nei] == -1:
-                        color[nei] = 1 - color[node]
-                        queue.append(nei)
-                    else:
-                        if color[nei] == color[node]:
-                            return False # conflict → not bipartite
+            for nei in graph[node]:
+                if color[nei] == -1:
+                    if not dfs(nei, 1 - c):
+                        return False
+                elif color[nei] == c:
+                    return False  # same color → odd cycle
 
-        return True        
+            return True
+
+        for i in range(1, n+1):
+            if color[i] == -1:
+                if not dfs(i, 0):
+                    return False  # odd cycle exists
+
+        return True  # no odd cycle → bipartite
