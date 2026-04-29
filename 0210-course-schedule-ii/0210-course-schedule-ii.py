befd7f344d94d98bb2f8657d51367e7740c9a161
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-30 01:22:27 +0530

    Time: 3 ms (75.47%), Space: 20.4 MB (74.72%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,3 +1,34 @@
+# class Solution:
+#     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+#         # build graph and indegree list
+#         graph = defaultdict(list)
+#         indegree = [0] * (numCourses)
+#         for u, v in prerequisites:
+#             graph[v].append(u)
+#             indegree[u] += 1
+
+#         # queue = deque([i for i in range(1, numCourses + 1) if indegree[i] == 0])
+        
+#         # use min-heap instead of queue
+#         min_heap  = []
+#         for i in range(numCourses):
+#             if indegree[i] == 0:
+#                 heapq.heappush(min_heap, i)
+        
+#         order = []
+
+#         while min_heap:
+#             node = heapq.heappop(min_heap)
+#             order.append(node)
+
+#             for nei in graph[node]:
+#                 indegree[nei] -= 1
+#                 if indegree[nei] == 0:
+#                     heapq.heappush(min_heap ,nei)
+
+#         return order if len(order) == numCourses else []
+
+
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         # build graph and indegree list
@@ -7,23 +38,16 @@ class Solution:
             graph[v].append(u)
             indegree[u] += 1
 
-        # queue = deque([i for i in range(1, numCourses + 1) if indegree[i] == 0])
-        
-        # use min-heap instead of queue
-        min_heap  = []
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                heapq.heappush(min_heap, i)
-        
+        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
         order = []
 
-        while min_heap:
-            node = heapq.heappop(min_heap)
+        while queue:
+            node = queue.popleft()
             order.append(node)
 
             for nei in graph[node]:
                 indegree[nei] -= 1
                 if indegree[nei] == 0:
-                    heapq.heappush(min_heap ,nei)
+                    queue.append(nei)
 
         return order if len(order) == numCourses else []
