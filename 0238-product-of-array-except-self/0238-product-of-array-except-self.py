5fc4d568187543e62d2b0b835d936e6c39c5d5b3
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-07-14 01:58:41 +0530

    Time: 22 ms (57.97%), Space: 25.2 MB (89.6%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,40 +1,61 @@
 # class Solution:
 #     def productExceptSelf(self, nums: List[int]) -> List[int]:
 #         n = len(nums)
-#         prefix_prod = [1] * n
-#         suffix_prod = [1] * n
-#         prefix_prod[0] = nums[0]
-#         suffix_prod[n-1] = nums[n-1]
+#         prefix = [1] * n
+#         suffix = [1] * n
+#         prefix[0] = nums[0]
+#         suffix[n-1] = nums[n-1]
 
 #         for i in range(1, n):
-#             prefix_prod[i] = prefix_prod[i-1] * nums[i]
+#             prefix[i] = prefix[i-1] * nums[i]
 
 #         for i in range(n-2, -1, -1):
-#             suffix_prod[i] = suffix_prod[i+1] * nums[i]    
+#             suffix[i] = suffix[i+1] * nums[i]    
 
 #         ans = []
 #         for i in range(len(nums)):
 #             if i == 0:
-#                 ans.append(suffix_prod[1])
+#                 ans.append(suffix[1])
 #             elif i == n-1:
-#                 ans.append(prefix_prod[n-2])
+#                 ans.append(prefix[n-2])
 #             else:
-#                 ans.append(prefix_prod[i-1] * suffix_prod[i+1])
+#                 ans.append(prefix[i-1] * suffix[i+1])
         
+#         return ans
+
+
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         n = len(nums)
+#         prefix = [1] * n
+#         suffix = [1] * n
+#         ans = [1] * n
+
+#         for i in range(1, n):
+#             prefix[i] = prefix[i - 1] * nums[i - 1]
+
+#         for i in range(n - 2, -1, -1):
+#             suffix[i] = suffix[i + 1] * nums[i + 1]
+
+#         for i in range(n):
+#             ans[i] = prefix[i] * suffix[i]
+
 #         return ans
 
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
+        ans = [1] * n
 
-        prefix = [1] * n
-        suffix = [1] * n
-
+        prefix = 1
         for i in range(1, n):
-            prefix[i] = prefix[i - 1] * nums[i - 1]
+            prefix = prefix * nums[i - 1]
+            ans[i] = prefix
 
+        suffix = 1
         for i in range(n - 2, -1, -1):
-            suffix[i] = suffix[i + 1] * nums[i + 1]
+            suffix *= nums[i + 1]
+            ans[i] *= suffix
 
-        return [prefix[i] * suffix[i] for i in range(n)]
+        return ans
