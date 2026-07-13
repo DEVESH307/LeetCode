7fd33e31b780d27e72909779e7414ed5abcf8c47
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-07-14 01:59:21 +0530

    Time: 0 ms (100%), Space: 20.3 MB (58.6%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,27 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         n = len(nums)
+
+#         for i in range(n):
+#             for j in range(i+1, n):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+        
+#         return []
+        
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash = {}
-        for i in range(len(nums)):
-            need = target - nums[i]
-            if need in hash:
-                return [hash[need],i]
-            hash[nums[i]] = i
+        n = len(nums)
+        res = []
+        idx_map = {}
+        
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in idx_map:
+                res.extend([idx_map[complement], i])
+                break
+            else:
+                idx_map[nums[i]] = i
+
+        return res
