70d8df985e0f388543e489454973678f78ec96f4
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-07-14 01:31:08 +0530

    Time: 35 ms (27.35%), Space: 27.7 MB (21.93%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,57 +1,40 @@
 # class Solution:
 #     def productExceptSelf(self, nums: List[int]) -> List[int]:
 #         n = len(nums)
-#         res = [0]*n
-
-#         for i in range(n):
-#             prod = 1
-#             for j in range(n):
-#                 if i != j:
-#                     prod *= nums[j]
-
-#             res[i] = prod
-        
-#         return res
-
-
-# class Solution:
-#     def productExceptSelf(self, nums: List[int]) -> List[int]:
-#         n = len(nums)
-#         res = [1]*n
-#         pref_mul = [1]*n
-#         suff_mul = [1]*n
-        
-#         pref_mul[0] = nums[0]
-#         suff_mul[n-1] = nums[n-1]
+#         prefix_prod = [1] * n
+#         suffix_prod = [1] * n
+#         prefix_prod[0] = nums[0]
+#         suffix_prod[n-1] = nums[n-1]
 
 #         for i in range(1, n):
-#             pref_mul[i] = pref_mul[i-1]*nums[i]
-#             suff_mul[n-i-1] = suff_mul[n-i]*nums[n-i-1]
+#             prefix_prod[i] = prefix_prod[i-1] * nums[i]
 
-#         for i in range(n):
+#         for i in range(n-2, -1, -1):
+#             suffix_prod[i] = suffix_prod[i+1] * nums[i]    
+
+#         ans = []
+#         for i in range(len(nums)):
 #             if i == 0:
-#                 res[0] = suff_mul[1]
+#                 ans.append(suffix_prod[1])
 #             elif i == n-1:
-#                 res[n-1] = pref_mul[n-2]
+#                 ans.append(prefix_prod[n-2])
 #             else:
-#                 res[i] = pref_mul[i-1]*suff_mul[i+1]
+#                 ans.append(prefix_prod[i-1] * suffix_prod[i+1])
         
-#         return res
+#         return ans
 
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        res = [1]*n
-        left = 1
 
-        for i in range(n):
-            res[i] = left
-            left *= nums[i]
+        prefix = [1] * n
+        suffix = [1] * n
 
-        right = 1
-        for i in range(n-1, -1, -1):
-            res[i] *= right
-            right *= nums[i]
-            
-        return res
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+
+        for i in range(n - 2, -1, -1):
+            suffix[i] = suffix[i + 1] * nums[i + 1]
+
+        return [prefix[i] * suffix[i] for i in range(n)]
