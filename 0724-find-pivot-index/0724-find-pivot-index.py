e0364ce6f92e6df85e1d8b2a00e9de2b9aef926a
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-07-15 00:34:14 +0530

    Time: 3 ms (90.08%), Space: 20.2 MB (97.3%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,20 +1,36 @@
+# class Solution:
+#     def pivotIndex(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         prefix_sum = [0] * n
+#         suffix_sum = [0] * n
+
+#         # Build prefix sums
+#         for i in range(1, n):
+#             prefix_sum[i] = prefix_sum[i - 1] + nums[i - 1]
+
+#         # Build suffix sums
+#         for i in range(n - 2, -1, -1):
+#             suffix_sum[i] = suffix_sum[i + 1] + nums[i + 1]
+
+#         # Find the pivot index
+#         for i in range(n):
+#             if prefix_sum[i] == suffix_sum[i]:
+#                 return i
+
+#         return -1
+
+
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         n = len(nums)
-        prefix_sum = [0] * n
-        suffix_sum = [0] * n
+        total_sum = sum(nums)
+        left_sum = 0
 
-        # Build prefix sums
-        for i in range(1, n):
-            prefix_sum[i] = prefix_sum[i - 1] + nums[i - 1]
-
-        # Build suffix sums
-        for i in range(n - 2, -1, -1):
-            suffix_sum[i] = suffix_sum[i + 1] + nums[i + 1]
-
-        # Find the pivot index
-        for i in range(n):
-            if prefix_sum[i] == suffix_sum[i]:
+        for i, num in enumerate(nums):
+            right_sum = total_sum - left_sum - num
+            if left_sum == right_sum:
                 return i
+
+            left_sum += num
 
         return -1
