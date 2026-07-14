89405b5f015c1417031a9632d104d165a4f7ed25
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-07-15 01:04:34 +0530

    Time: 0 ms (100%), Space: 19.3 MB (23.65%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -1,27 +1,49 @@
+# class Solution:
+#     def summaryRanges(self, nums: List[int]) -> List[str]:
+#         if not nums:
+#             return []
+
+#         n = len(nums)
+#         start = end = nums[0]
+#         res = []
+
+#         for i in range(1, n):
+#             if end + 1 == nums[i]:
+#                 end = nums[i]
+#             else:            
+#                 if start == end:
+#                     res.append(str(start))
+#                 else:
+#                     res.append(str(start)+"->"+str(end))
+                    
+#                 start = nums[i]
+#                 end = nums[i]
+
+#         if start == end:
+#             res.append(str(start))
+#         else:
+#             res.append(str(start)+"->"+str(end))
+
+#         return res
+
+
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        if not nums:
-            return []
-            
-        n = len(nums)
-        start = end = nums[0]
         res = []
+        n = len(nums)
+        i = 0
 
-        for i in range(1, n):
-            if end + 1 == nums[i]:
-                end = nums[i]
-            else:            
-                if start == end:
-                    res.append(str(start))
-                else:
-                    res.append(str(start)+"->"+str(end))
-                    
-                start = nums[i]
-                end = nums[i]
+        while i < n:
+            start = nums[i]
 
-        if start == end:
-            res.append(str(start))
-        else:
-            res.append(str(start)+"->"+str(end))
+            while i + 1 < n and nums[i + 1] == nums[i] + 1:
+                i += 1
+
+            if start == nums[i]:
+                res.append(str(start))
+            else:
+                res.append(f"{start}->{nums[i]}")
+
+            i += 1
 
         return res
