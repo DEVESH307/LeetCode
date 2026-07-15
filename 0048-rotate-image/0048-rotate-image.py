b532f40828b1f70ddfb00df473b34e5d3f637b1f
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-07-16 01:35:00 +0530

    Time: 0 ms (100%), Space: 19.2 MB (92.75%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,31 +1,27 @@
 class Solution:
-    def transpose(self, mat):
-        n = len(mat)
-
-        for i in range(n):
-            for j in range(i+1, n):
-                mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
-
-    def reverse_rows(self, mat):
-        n = len(mat)
-        m = len(mat[0])
-        
-        for i in range(n):
-            left = 0
-            right = m - 1
-            
-            while left < right:
-                mat[i][left], mat[i][right] = mat[i][right], mat[i][left]
-                left += 1
-                right -= 1
-
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        n = len(matrix)
+        def transpose():
+            n = len(matrix)
 
-        self.transpose(matrix)
-        self.reverse_rows(matrix)
+            for i in range(n):
+                for j in range(i+1, n):
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-        return matrix
+        def reverse_rows():
+            n = len(matrix)
+            m = len(matrix[0])
+            
+            for i in range(n):
+                left = 0
+                right = m - 1
+                
+                while left < right:
+                    matrix[i][left], matrix[i][right] = matrix[i][right], matrix[i][left]
+                    left += 1
+                    right -= 1
+
+        transpose()
+        reverse_rows()
