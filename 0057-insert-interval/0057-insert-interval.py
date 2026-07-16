37bd6201147b1306c895414cd6a1a7339c6c3a8c
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-07-17 00:54:48 +0530

    Time: 0 ms (100%), Space: 21.4 MB (57.23%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,22 +1,49 @@
+# class Solution:
+#     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+#         ni_start, ni_end = newInterval
+#         res = []
+
+#         for i, (start, end) in enumerate(intervals):
+#             if end < ni_start:
+#                 res.append([start, end])
+
+#             elif start > ni_end:
+#                 res.append([ni_start, ni_end])
+
+#                 # Append the remaining intervals
+#                 for j in range(i, len(intervals)):
+#                     res.append(intervals[j])
+
+#                 return res
+
+#             else:
+#                 ni_start = min(ni_start, start)
+#                 ni_end = max(ni_end, end)
+
+#         # newInterval belongs at the end
+#         res.append([ni_start, ni_end])
+#         return res
+
+
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         ni_start, ni_end = newInterval
-        result = []
+        res = []
         inserted = False
 
         for start, end in intervals:
             if end < ni_start:
-                result.append([start, end])
+                res.append([start, end])
             elif start > ni_end:
                 if not inserted:
-                    result.append([ni_start, ni_end])
+                    res.append([ni_start, ni_end])
                     inserted = True
-                result.append([start, end])
+                res.append([start, end])
             else:
                 ni_start = min(ni_start, start)
                 ni_end = max(ni_end, end)
 
         if not inserted:
-            result.append([ni_start, ni_end])
+            res.append([ni_start, ni_end])
 
-        return result
+        return res
