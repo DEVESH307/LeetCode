ad16063d84d8a9fb660ef137c3bfa64c03ac78a1
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-07-17 03:34:47 +0530

    Time: 223 ms (16.55%), Space: 45.2 MB (57.52%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,29 +1,24 @@
-# class Solution:
-#     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-#         intervals.sort(key=lambda x: x[0])
-#         prevEnd = intervals[0][1]
-#         ans = 0
-
-#         for start, end in intervals[1:]:
-#             if start >= prevEnd:
-#                 prevEnd = end
-#             else:
-#                 ans += 1
-#                 prevEnd = min(prevEnd, end)
-
-#         return ans
-         
-
-class Solution:
-    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key=lambda x: x[1])  # sort by end time
-        
+class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        if not intervals:
+            return 0
+        intervals.sort(key=lambda x: x[1])
+        start, end = intervals[0]
+                            
+        intervals.sort(key=lambda x: x[0])
+        last_end = intervals[0][1]
         count = 0
-        last_end = float('-inf')
-        
-        for start, end in intervals:
-            if start >= last_end:
-                count += 1
+
+        for interval in intervals[1:]:
+            start, end = interval[0], interval[1]
+            if last_end <= start:
                 last_end = end
+            else:
+                count += 1
+                last_end = min(last_end, end)
         
-        return len(intervals) - count
+        return count
