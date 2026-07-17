78158d87166f3ccab6e25ac5bfcf8cb4a6b92155
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-07-18 02:05:48 +0530

    Time: 15 ms (79.44%), Space: 20.7 MB (5.78%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -41,6 +41,23 @@
 # class Solution:
 #     def canJump(self, nums: List[int]) -> bool:
 #         n = len(nums)
+#         dp = [False] * n
+#         dp[-1] = True
+
+#         for i in range(n - 2, -1, -1):
+#             farthest = min(i + nums[i], n - 1)
+
+#             for j in range(i + 1, farthest + 1):
+#                 if dp[j]:
+#                     dp[i] = True
+#                     break
+
+#         return dp[0]
+
+
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         n = len(nums)
 #         goal = n - 1
 
 #         for i in range(n - 2, -1, -1):
@@ -61,4 +78,3 @@ class Solution:
 
             farthest = max(farthest, i + nums[i])
         return True
-
