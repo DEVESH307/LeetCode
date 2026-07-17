b5dc56893e10fae23ccd7834523d8450ba3ae5b6
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-07-17 18:34:22 +0530

    Time: 23 ms (42.96%), Space: 14.7 MB (50.49%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,16 +1,18 @@
-class Solution:
-    def candy(self, ratings: List[int]) -> int:
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
         n = len(ratings)
         candies = [1] * n
 
-        # leftr -> right
         for i in range(1, n):
             if ratings[i] > ratings[i-1]:
                 candies[i] = candies[i-1] + 1
 
-        # right to left
         for i in range(n-2, -1, -1):
             if ratings[i] > ratings[i+1]:
-                candies[i] = max(candies[i], candies[i+1]+1)
+                candies[i] = max(candies[i], candies[i+1] + 1)
 
-        return sum(candies)        
+        return sum(candies)
