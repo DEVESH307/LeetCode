d248579158e0b371e22817ccd85e6905629e9f15
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-07-31 15:20:00 +0530

    Time: 3 ms (51.12%), Space: 19.4 MB (23.05%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,10 +1,23 @@
+# class Solution:
+#     def findTheDifference(self, s: str, t: str) -> str:
+#         s = sorted(s)
+#         t = sorted(t)
+
+#         for i in range(len(s)):
+#             if s[i] != t[i]:
+#                 return t[i]
+
+#         return t[-1]
+
+
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s = sorted(s)
-        t = sorted(t)
+        freq = {}
 
-        for i in range(len(s)):
-            if s[i] != t[i]:
-                return t[i]
+        for ch in s:
+            freq[ch] = freq.get(ch, 0) + 1
 
-        return t[-1]
+        for ch in t:
+            if ch not in freq or freq[ch] == 0:
+                return ch
+            freq[ch] -= 1
