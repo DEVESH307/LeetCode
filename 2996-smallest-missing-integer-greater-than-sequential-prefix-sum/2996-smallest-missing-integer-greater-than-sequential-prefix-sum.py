fff8194eafb34d99c4f308488f9ce0b172610aa4
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-08-11 13:18:31 +0530

    Time: 4 ms (5.08%), Space: 19.5 MB (17.68%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
@@ -1,18 +1,34 @@
+# class Solution:
+#     def missingInteger(self, nums: List[int]) -> int:
+#         prefix_sum = nums[0]
+
+#         for i in range(1, len(nums)):
+#             if nums[i] != nums[i - 1] + 1:
+#                 break
+#             prefix_sum += nums[i]
+
+#         seen = set(nums)
+
+#         while prefix_sum in seen:
+#             prefix_sum += 1
+
+#         return prefix_sum
+
+
 class Solution:
     def missingInteger(self, nums: List[int]) -> int:
-        prefix_sum = [nums[0]]
-        
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i-1] + 1:
-                break
-            prefix_sum.append(prefix_sum[i-1] + nums[i])
+        # 1. Find sum of longest consecutive prefix
+        prefix_sum = nums[0]
 
-        # print(prefix_sum)
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1] + 1:
+                break
+            prefix_sum += nums[i]
+
+        # 2. Find smallest integer >= total that isn't in nums
         seen = set(nums)
-        num = prefix_sum[-1]
-        # print(num)
-        while True:
-            if num not in seen:
-                return num
-            num += 1
-        
+
+        while prefix_sum in seen:
+            prefix_sum += 1
+
+        return prefix_sum
