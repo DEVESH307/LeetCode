d8624ceda2f3c879aae4df031fed640ae9d06737
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-08-13 02:33:37 +0530

    Time: 579 ms (73.19%), Space: 20.1 MB (61.38%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.py
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -1,21 +1,57 @@
-from functools import cache
+# from functools import cache
+# class Solution:
+#     def winnerSquareGame(self, n: int) -> bool:
+
+#         @cache
+#         def dfs(i):
+#             if i == 0:
+#                 return False
+
+#             j = 1
+#             while j * j <= i:
+#                 if not dfs(i - j * j):
+#                     return True
+#                 j += 1
+
+#             return False
+     
+#         return dfs(n)
+        
+
+# class Solution:
+#     def winnerSquareGame(self, n: int) -> bool:
+#         dp = [-1] * (n + 1)
+
+#         def dfs(i):
+#             if i == 0:
+#                 return False
+
+#             if dp[i] != -1:
+#                 return dp[i]
+
+#             j = 1
+#             while j * j <= i:
+#                 if not dfs(i - j * j):
+#                     dp[i] = True
+#                     return True
+#                 j += 1
+
+#             dp[i] = False
+#             return False
+     
+#         return dfs(n)
+        
+
 class Solution:
     def winnerSquareGame(self, n: int) -> bool:
-        ans = float('inf')
+        dp = [False] * (n + 1)
 
-        @cache
-        def dfs(i):
-            nonlocal ans
-            if i == 0:
-                return False
-
+        for i in range(1, n + 1):
             j = 1
             while j * j <= i:
-                if not dfs(i - j * j):
-                    return True
+                if not dp[i - j * j]:
+                    dp[i] = True
+                    break
                 j += 1
 
-            return False
-     
-        return dfs(n)
-        
+        return dp[n]        
