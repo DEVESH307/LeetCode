1bf9999b3e95f32b2b1bd6f865bda3e6e6501bf5
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-08-14 17:30:38 +0530

    Time: 28 ms (12.23%), Space: 28.5 MB (5.55%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,19 +1,39 @@
-import heapq
+# import heapq
+
+# class Solution:
+#     def frequencySort(self, s: str) -> str:
+#         freq = {}
+
+#         for ch in s:
+#             freq[ch] = freq.get(ch, 0) + 1
+
+#         max_heap = [(-count, ch) for ch, count in freq.items()]
+#         heapq.heapify(max_heap)
+
+#         result = []
+
+#         while max_heap:
+#             count, ch = heapq.heappop(max_heap)
+#             result.append(ch * (-count))
+
+#         return "".join(result)
+
+
+from collections import Counter
 
 class Solution:
     def frequencySort(self, s: str) -> str:
-        freq = {}
+        freq = Counter(s)
 
-        for i, ch in enumerate(s):
-            freq[ch] = freq.get(ch, 0) + 1
+        buckets = [[] for _ in range(len(s) + 1)]
 
-        max_heap = [(-val, key) for key, val in freq.items()]
-        heapq.heapify(max_heap)
+        for ch, count in freq.items():
+            buckets[count].append(ch)
 
-        res = []
-        while max_heap:
-            neg_val, key = heapq.heappop(max_heap)
-            res.append(-neg_val * key)
+        result = []
 
-        print(res)
-        return "".join(res)
+        for count in range(len(s), 0, -1):
+            for ch in buckets[count]:
+                result.append(ch * count)
+
+        return "".join(result)
