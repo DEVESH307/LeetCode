1a311a98ece35602110f16bda8c0e9a2f999c472
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-08-20 14:25:59 +0530

    Time: 31 ms (77.18%), Space: 22.8 MB (62.82%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -1,22 +1,52 @@
+# from collections import defaultdict
+
+# class Solution:
+#     def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
+#         reserved = defaultdict(set)
+
+#         for row, seat in reservedSeats:
+#             reserved[row].add(seat)
+
+#         ans = (n - len(reserved)) * 2
+
+#         for seats in reserved.values():
+#             left = all(x not in seats for x in range(2, 6))
+#             middle = all(x not in seats for x in range(4, 8))
+#             right = all(x not in seats for x in range(6, 10))
+
+#             if left and right:
+#                 ans += 2
+#             elif left or middle or right:
+#                 ans += 1
+
+#         return ans
+
+
 from collections import defaultdict
 
 class Solution:
-    def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
-        reserved = defaultdict(set)
+    def maxNumberOfFamilies(self, n, reservedSeats):
+        rows = defaultdict(int)
 
         for row, seat in reservedSeats:
-            reserved[row].add(seat)
+            if 2 <= seat <= 9:
+                rows[row] |= 1 << (seat - 2)
 
-        ans = (n - len(reserved)) * 2
+        LEFT  = 0b00001111
+        MID   = 0b00111100
+        RIGHT = 0b11110000
 
-        for seats in reserved.values():
-            left = all(x not in seats for x in range(2, 6))
-            middle = all(x not in seats for x in range(4, 8))
-            right = all(x not in seats for x in range(6, 10))
+        ans = (n - len(rows)) * 2
+
+        for mask in rows.values():
+
+            left = (mask & LEFT) == 0
+            mid = (mask & MID) == 0
+            right = (mask & RIGHT) == 0
 
             if left and right:
                 ans += 2
-            elif left or middle or right:
+            elif left or mid or right:
                 ans += 1
 
         return ans
