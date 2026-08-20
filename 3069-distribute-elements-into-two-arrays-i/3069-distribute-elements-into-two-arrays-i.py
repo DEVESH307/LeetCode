04b1a4a5ae9a0c95e64c55665c15cf30c43ee6ac
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-08-20 14:20:00 +0530

    Time: 0 ms (100%), Space: 19.4 MB (22.76%) - LeetHub

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.py
@@ -0,0 +1,17 @@
+class Solution:
+    def resultArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        if n <= 2:
+            return nums
+
+        arr1 = [nums[0]]
+        arr2 = [nums[1]]
+
+        for num in nums[2:]:
+            if arr1[-1] > arr2[-1]:
+                arr1.append(num)
+            else:
+                arr2.append(num)
+
+        return arr1 + arr2
